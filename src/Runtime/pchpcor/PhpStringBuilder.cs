680f07358348d039cefa3850e03cfaac6452ac1d
Jakub Míšek <jmisek@gmail.com>
2016-03-14 15:08:29 +0100

    PhpStringBuilder concept

--- src/Runtime/pchpcor/PhpStringBuilder.cs
+++ src/Runtime/pchpcor/PhpStringBuilder.cs
@@ -12,6 +12,33 @@ namespace Pchp.Core
     /// </summary>
     public class PhpStringBuilder
     {
+        #region Fields & Properties
 
+        /// <summary>
+        /// Bit mask of chunks containing <see cref="BinaryString"/> or <see cref="StringBuilder"/>, otherwise it is <see cref="string"/>.
+        /// </summary>
+        ulong _writable = 0;
+
+        /// <summary>
+        /// Max count of chunks.
+        /// Adding chunks to full builder causes all chunks to be concatenated.
+        /// </summary>
+        const int MaxChunks = sizeof(ulong);
+
+        #endregion
+
+        #region Construction
+
+        // from builder, binary, unicode
+
+        #endregion
+
+        #region Operations
+
+        // Append
+        // Prepend
+        // this[] { get; set; }
+
+        #endregion
     }
 }
