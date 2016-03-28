dc6f943b932dffc97af93e9b43ab9e7b4775786b
Jakub Míšek <jmisek@gmail.com>
2016-03-28 15:27:47 +0200

    Context.Echo

---
+++ src/Runtime/pchpcor/Context.Output.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pchp.Core
+{
+    partial class Context
+    {
+        // TODO: output filter stack
+        // TOOD: CreateConsoleOutputFilter
+        // TODO: echo using current output filter
+
+        #region Echo
+
+        public void Echo(object value)
+        {
+            if (value != null)
+                Echo(value.ToString());
+        }
+
+        public void Echo(string value)
+        {
+            if (value != null)
+                ConsoleImports.Write(value);
+        }
+
+        public void Echo(PhpString value)
+        {
+            Echo(value.ToString(this));    // TODO: echo string builder chunks to avoid concatenation
+        }
+
+        public void Echo(PhpValue value)
+        {
+            ConsoleImports.Write(value.ToString(this));
+        }
+
+        public void Echo(PhpNumber value)
+        {
+            if (value.IsLong)
+                Echo(value.Long);
+            else
+                Echo(value.Double);
+        }
+
+        public void Echo(double value)
+        {
+            ConsoleImports.Write(Convert.ToString(value, this));
+        }
+
+        public void Echo(long value)
+        {
+            ConsoleImports.Write(value.ToString());
+        }
+
+        public void Echo(int value)
+        {
+            ConsoleImports.Write(value.ToString());
+        }
+
+        #endregion
+    }
+}
--- src/Runtime/pchpcor/Context.cs
+++ src/Runtime/pchpcor/Context.cs
@@ -15,7 +15,7 @@ namespace Pchp.Core
     /// Its instance is passed to all PHP function.
     /// The context is not thread safe.
     /// </remarks>
-    public class Context : IDisposable
+    public partial class Context : IDisposable
     {
         #region Create
 
@@ -38,55 +38,6 @@ namespace Pchp.Core
         public void Dispose()
         {
 
-        }
-
-        #endregion
-
-        #region Echo
-
-        public void Echo(object value)
-        {
-            if (value != null)
-                Echo(value.ToString());
-        }
-
-        public void Echo(string value)
-        {
-            if (value != null)
-                ConsoleImports.Write(value);
-        }
-
-        public void Echo(PhpString value)
-        {
-            Echo(value.ToString(this));    // TODO: echo string builder chunks to avoid concatenation
-        }
-
-        public void Echo(PhpValue value)
-        {
-            ConsoleImports.Write(value.ToString(this));
-        }
-
-        public void Echo(PhpNumber value)
-        {
-            if (value.IsLong)
-                Echo(value.Long);
-            else
-                Echo(value.Double);
-        }
-
-        public void Echo(double value)
-        {
-            ConsoleImports.Write(Convert.ToString(value, this));
-        }
-
-        public void Echo(long value)
-        {
-            ConsoleImports.Write(value.ToString());
-        }
-
-        public void Echo(int value)
-        {
-            ConsoleImports.Write(value.ToString());
         }
 
         #endregion
