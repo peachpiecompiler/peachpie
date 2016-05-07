1cda33d089cdfbe84ab944bbdc0b05839324f7fe
Jakub Míšek <jmisek@gmail.com>
2016-05-08 00:46:37 +0200

    switch test

---
+++ tests/constructs/switch_001.php
@@ -0,0 +1,32 @@
+<?php
+
+/** @param int $a */
+function test($a)
+{
+	switch ($a)
+	{
+		case 1:
+			echo "1";
+			break;
+		case 2:
+			echo "2";
+			//break;
+		case 3:
+			echo "3";
+			break;
+		case 4:
+			echo "4";
+			//break;
+		case 123:
+			echo "123";
+			//break;
+		case 456:
+			echo "456";
+			break;
+		default:
+			echo "default";
+			break;
+	}
+}
+
+test(123);
