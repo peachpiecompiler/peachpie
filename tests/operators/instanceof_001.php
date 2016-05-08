43144e4ecf458187b144ee9b89ce7022fa098fac
Jakub Míšek <jmisek@gmail.com>
2016-05-08 12:18:58 +0200

    test - instanceof

---
+++ tests/operators/instanceof_001.php
@@ -0,0 +1,34 @@
+<?php
+
+class Dog {
+	
+}
+
+class BigDog extends Dog {
+	
+}
+
+class Cat {
+	
+}
+
+function test($x) {
+	echo $x instanceof Dog ? "1" : "0";
+}
+
+/** @param object */
+function test2($x) {
+	echo $x instanceof Dog ? "1" : "0";
+}
+
+test(new Dog);
+test(new BigDog);
+test(new Cat);
+test2(new Dog);
+test2(new Cat);
+test(null);
+test(123);
+test(123.456);
+test("Dog");
+test([1,2,3]);
+test(true);
