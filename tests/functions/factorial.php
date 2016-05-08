4addd218797d4730f15d356716fea6dfed03a7a2
Jakub Míšek <jmisek@gmail.com>
2016-05-08 12:28:46 +0200

    tests - factorial
    - recursion

---
+++ tests/functions/factorial.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @param int $number
+ */
+function factorial($number)
+{
+    if ($number < 2) {
+        return 1;
+    } else {
+        return factorial($number - 1) * $number;
+    }
+}
+
+echo factorial(4), ', ',
+	 factorial(8), ', ',
+	 factorial(16);
