24798cfc69889568c10a748cd8f03d58fe252c35
Jakub Míšek <jmisek@gmail.com>
2016-05-10 15:07:25 +0200

    tests - stdClass

---
+++ tests/classes/stdClass.php
@@ -0,0 +1,17 @@
+<?php
+
+function test() {
+	$x = new stdClass;
+	$x->a = 1;
+	$x->b = 2;
+	echo $x->a, $x->b;
+	
+	$y = null;
+	@$y->a = 3;
+	@$y->b = 4;
+	echo $x->a, $x->b;
+}
+
+test();
+
+echo "Done.";
