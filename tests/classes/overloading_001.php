4f9011fb0a5c57dfa8bdeedfba43ffe20cbc29d6
Jakub Míšek <jmisek@gmail.com>
2016-05-10 15:07:15 +0200

    tests - classes

---
+++ tests/classes/overloading_001.php
@@ -0,0 +1,24 @@
+<?php
+
+class Dog {
+	function Whof(){ echo "whof"; }
+}
+
+class BigDog {
+	function Whof(){ echo "WHOF!"; }
+}
+
+function test($dog) {
+	$dog->Whof();
+}
+
+function test2() {
+	$dog = new BigDog;
+	$dog->Whof();
+}
+
+test(new Dog);
+test(new BigDog);
+test2();
+
+echo "Done.";
