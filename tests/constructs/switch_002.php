d9a8474c6d275935b63aae4a96769a48a3308da2
Jakub Míšek <jmisek@gmail.com>
2016-05-17 13:18:02 +0200

    tests - switch, equals
    - switch testing various equality operand types

---
+++ tests/constructs/switch_002.php
@@ -0,0 +1,26 @@
+<?php
+
+function test($a)
+{
+	switch ($a)	// $a is mixed
+	{
+		case 1: return "1";
+		case 2: return "2";
+		case 123: return "123";
+		case false: return "false";
+		case "hello": return "hello";
+		case "Hello": return "Hello";
+		case 1.0: return "1.0";
+		case 1.2: return 1.2;
+		case true: return "true";
+		default: return "default";
+	}
+}
+
+echo test(123);
+echo test(1.2);
+echo test(false);
+echo test(true);
+echo test("Hello");
+
+echo "Done.";
