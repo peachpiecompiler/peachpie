b1cc61148d15b8a1e8a9b44ee40329f8a939a8fc
Jakub Míšek <jmisek@gmail.com>
2016-05-26 16:29:05 +0200

    tests - Strict Equality

--- tests/operators/strictequality_001.php
+++ tests/operators/strictequality_001.php
@@ -1,12 +1,23 @@
 <?php
 
+function b($b) {
+	echo $b ? 1 : 0;
+}
+
 function test() {
 	$b = 1.0 < 2.0;
 
-	echo ($b) ? 1 : 0;
-	echo ($b == 1) ? 1 : 0;
-	echo ($b === 1) ? 1 : 0;
-	echo ($b === true) ? 1 : 0;
+	b($b);
+	b($b == 1);
+	b($b === 1);
+	b($b === true);
+	b(true === $b);
+	b(false === $b);
+	b(1 === $b);
+	b($b === 0.0);
+	b(1.0 === $b);
+	b("1" === $b);
+	b($b === "1");
 }
 
 test();
