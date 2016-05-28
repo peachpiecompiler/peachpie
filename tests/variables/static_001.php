20574afd31ec779608a1307df441a78a9b6bce39
Jakub Míšek <jmisek@gmail.com>
2016-05-28 17:19:02 +0200

    tests - static local

---
+++ tests/variables/static_001.php
@@ -0,0 +1,27 @@
+<?php
+
+function test()
+{
+	static $x;
+	static $y = 123;
+	static $loaded = false;
+	
+	if (!$loaded) {
+		$loaded = true;
+		echo "First";
+	}
+	
+	$x ++;
+	$y ++;
+	
+	echo $x, $y;
+}
+
+test();
+test();
+test();
+test();
+test();
+test();
+
+echo "Done.";
