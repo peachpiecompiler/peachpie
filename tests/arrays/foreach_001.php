e4d8db94385eb389ade61dc83768b5aa5d39126a
Jakub Míšek <jmisek@gmail.com>
2016-06-04 14:59:30 +0200

    tests - array foreach

---
+++ tests/arrays/foreach_001.php
@@ -0,0 +1,15 @@
+<?php
+
+function test1() {
+	$arr = [1,2,3,4,5];
+	
+	foreach ($arr as $value) {
+		echo $value, "; ";
+	}
+	
+	foreach ($arr as $key => $value) {
+		echo $key, " => ", $value, "; ";
+	}
+}
+
+test1();
