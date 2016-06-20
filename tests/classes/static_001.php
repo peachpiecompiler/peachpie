e5b0f85795489c80987ce0b7cbbcc0835e04bf83
Jakub Míšek <jmisek@gmail.com>
2016-06-20 14:55:26 +0200

    tests - static fields & appstatic fields

---
+++ tests/classes/static_001.php
@@ -0,0 +1,27 @@
+<?php
+
+class X {
+
+	static $sfld1;
+	
+	/** @var int */
+	static $sfld2;
+	
+	/** @appstatic */
+	static $sfld3;
+	
+	static function test1() {
+		X::$sfld1 = "Helo";
+		X::$sfld2 ++;
+		X::$sfld3 = "World";
+	}
+	
+	static function test2() {
+		echo X::$sfld1, X::$sfld2, X::$sfld3;
+	}
+}
+
+X::test1();
+X::test2();
+
+echo "Done.";
