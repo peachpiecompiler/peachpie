c938ac114087544f4342b338482b6b5197252c42
Jakub Míšek <jmisek@gmail.com>
2016-07-07 13:10:18 +0200

    str_pad

--- src/Runtime/pchplib/Strings.cs
+++ src/Runtime/pchplib/Strings.cs
@@ -8,6 +8,213 @@ namespace Pchp.Library
 {
     public static class Strings
     {
+        #region str_pad
+
+        /// <summary>
+        /// Type of padding.
+        /// </summary>
+        public enum PaddingType
+        {
+            /// <summary>Pad a string from the left.</summary>
+            Left = 0,
+
+            /// <summary>Pad a string from the right.</summary>
+            Right = 1,
+
+            /// <summary>Pad a string from both sides.</summary>
+            Both = 2
+        }
+
+        public const int STR_PAD_LEFT = (int)PaddingType.Left;
+        public const int STR_PAD_RIGHT = (int)PaddingType.Right;
+        public const int STR_PAD_BOTH = (int)PaddingType.Both;
+
+        /// <summary>
+        /// Pads a string to a certain length with spaces.
+        /// </summary>
+        /// <param name="str">The string to pad.</param>
+        /// <param name="totalWidth">Desired length of the returned string.</param>
+        /// <returns><paramref name="str"/> padded on the right with spaces.</returns>
+        public static string str_pad(string str, int totalWidth)
+        {
+            //if (str is PhpBytes)
+            //    return Pad(str, totalWidth, new PhpBytes(32));
+            //else
+                return str_pad(str, totalWidth, " ");
+        }
+
+        /// <summary>
+        /// Pads a string to certain length with another string.
+        /// </summary>
+        /// <param name="str">The string to pad.</param>
+        /// <param name="totalWidth">Desired length of the returned string.</param>
+        /// <param name="paddingString">The string to use as the pad.</param>
+        /// <returns><paramref name="str"/> padded on the right with <paramref name="paddingString"/>.</returns>
+        /// <exception cref="PhpException">Thrown if <paramref name="paddingString"/> is null or empty.</exception>
+        public static string str_pad(string str, int totalWidth, string paddingString)
+        {
+            return str_pad(str, totalWidth, paddingString, PaddingType.Right);
+        }
+
+        /// <summary>
+        /// Pads a string to certain length with another string.
+        /// </summary>
+        /// <param name="str">The string to pad.</param>
+        /// <param name="totalWidth">Desired length of the returned string.</param>
+        /// <param name="paddingString">The string to use as the pad.</param>
+        /// <param name="paddingType">Specifies whether the padding should be done on the left, on the right,
+        /// or on both sides of <paramref name="str"/>.</param>
+        /// <returns><paramref name="str"/> padded with <paramref name="paddingString"/>.</returns>
+        /// <exception cref="PhpException">Thrown if <paramref name="paddingType"/> is invalid or <paramref name="paddingString"/> is null or empty.</exception>
+        public static string str_pad(string str, int totalWidth, string paddingString, PaddingType paddingType)
+        {
+            //PhpBytes binstr = str as PhpBytes;
+            //if (str is PhpBytes)
+            //{
+            //    PhpBytes binPaddingString = Core.Convert.ObjectToPhpBytes(paddingString);
+
+            //    if (binPaddingString == null || binPaddingString.Length == 0)
+            //    {
+            //        PhpException.InvalidArgument("paddingString", LibResources.GetString("arg:null_or_empty"));
+            //        return null;
+            //    }
+            //    if (binstr == null) binstr = PhpBytes.Empty;
+
+            //    int length = binstr.Length;
+            //    if (totalWidth <= length) return binstr;
+
+            //    int pad = totalWidth - length, padLeft = 0, padRight = 0;
+
+            //    switch (paddingType)
+            //    {
+            //        case PaddingType.Left: padLeft = pad; break;
+            //        case PaddingType.Right: padRight = pad; break;
+
+            //        case PaddingType.Both:
+            //            padLeft = pad / 2;
+            //            padRight = pad - padLeft;
+            //            break;
+
+            //        default:
+            //            PhpException.InvalidArgument("paddingType");
+            //            break;
+            //    }
+
+            //    // if paddingString has length 1, use String.PadLeft and String.PadRight
+            //    int padStrLength = binPaddingString.Length;
+
+            //    // else build the resulting string manually
+            //    byte[] result = new byte[totalWidth];
+
+            //    int position = 0;
+
+            //    // pad left
+            //    while (padLeft > padStrLength)
+            //    {
+            //        Buffer.BlockCopy(binPaddingString.ReadonlyData, 0, result, position, padStrLength);
+            //        padLeft -= padStrLength;
+            //        position += padStrLength;
+            //    }
+
+            //    if (padLeft > 0)
+            //    {
+            //        Buffer.BlockCopy(binPaddingString.ReadonlyData, 0, result, position, padLeft);
+            //        position += padLeft;
+            //    }
+
+            //    Buffer.BlockCopy(binstr.ReadonlyData, 0, result, position, binstr.Length);
+            //    position += binstr.Length;
+
+            //    // pad right
+            //    while (padRight > padStrLength)
+            //    {
+            //        Buffer.BlockCopy(binPaddingString.ReadonlyData, 0, result, position, padStrLength);
+            //        padRight -= padStrLength;
+            //        position += padStrLength;
+            //    }
+
+            //    if (padRight > 0)
+            //    {
+            //        Buffer.BlockCopy(binPaddingString.ReadonlyData, 0, result, position, padRight);
+            //        position += padRight;
+            //    }
+
+            //    return new PhpBytes(result);
+            //}
+
+            string unistr = str; // Core.Convert.ObjectToString(str);
+            if (unistr != null)
+            {
+                string uniPaddingString = paddingString; // Core.Convert.ObjectToString(paddingString);
+
+                if (string.IsNullOrEmpty(uniPaddingString))
+                {
+                    //PhpException.InvalidArgument("paddingString", LibResources.GetString("arg:null_or_empty"));
+                    //return null;
+                    throw new ArgumentException();
+                }
+
+                int length = unistr.Length;
+                if (totalWidth <= length) return unistr;
+
+                int pad = totalWidth - length, padLeft = 0, padRight = 0;
+
+                switch (paddingType)
+                {
+                    case PaddingType.Left: padLeft = pad; break;
+                    case PaddingType.Right: padRight = pad; break;
+
+                    case PaddingType.Both:
+                        padLeft = pad / 2;
+                        padRight = pad - padLeft;
+                        break;
+
+                    default:
+                        //PhpException.InvalidArgument("paddingType");
+                        //break;
+                        throw new ArgumentException();
+                }
+
+                // if paddingString has length 1, use String.PadLeft and String.PadRight
+                int padStrLength = uniPaddingString.Length;
+                if (padStrLength == 1)
+                {
+                    char c = uniPaddingString[0];
+                    if (padLeft > 0) unistr = unistr.PadLeft(length + padLeft, c);
+                    if (padRight > 0) unistr = unistr.PadRight(totalWidth, c);
+
+                    return unistr;
+                }
+
+                // else build the resulting string manually
+                StringBuilder result = new StringBuilder(totalWidth);
+
+                // pad left
+                while (padLeft > padStrLength)
+                {
+                    result.Append(uniPaddingString);
+                    padLeft -= padStrLength;
+                }
+                if (padLeft > 0) result.Append(uniPaddingString.Substring(0, padLeft));
+
+                result.Append(unistr);
+
+                // pad right
+                while (padRight > padStrLength)
+                {
+                    result.Append(uniPaddingString);
+                    padRight -= padStrLength;
+                }
+                if (padRight > 0) result.Append(uniPaddingString.Substring(0, padRight));
+
+                return result.ToString();
+            }
+
+            return null;
+        }
+
+        #endregion
+
         /// <summary>
         /// Reverses the given string.
         /// </summary>
