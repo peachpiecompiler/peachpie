e03d3e8ac49d231d7c3332c493c5e5d1709e3636
Jakub Míšek <jmisek@gmail.com>
2016-08-11 17:01:24 +0200

    array functions
    - array_pop, array_push, array_shift, array_unshift, array_reverse

--- src/Runtime/pchplib/Arrays.cs
+++ src/Runtime/pchplib/Arrays.cs
@@ -281,5 +281,169 @@ namespace Pchp.Library
         }
 
         #endregion
+
+        #region array_pop, array_push, array_shift, array_unshift, array_reverse
+
+
+        /// <summary>
+        /// Removes the last item from an array and returns it.
+        /// </summary>
+        /// <param name="array">The array whcih item to pop.</param>
+        /// <returns>The last item of <paramref name="array"/> or a <b>null</b> reference if it is empty.</returns>
+        /// <remarks>Resets intrinsic enumerator.</remarks>
+        public static PhpValue array_pop(PhpArray array)
+        {
+            if (array == null)
+            {
+                //PhpException.ReferenceNull("array");
+                //return null;
+                throw new ArgumentNullException();
+            }
+
+            if (array.Count == 0) return PhpValue.Null;
+
+            // dereferences result since the array doesn't do so:
+            var result = (array.RemoveLast().Value);    // TODO: PhpVariable.Dereference
+
+            array.RefreshMaxIntegerKey();
+            array.RestartIntrinsicEnumerator();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adds multiple items into an array.
+        /// </summary>
+        /// <param name="array">The array where to add values.</param>
+        /// <param name="vars">The array of values to add.</param>
+        /// <returns>The number of items in array after all items was added.</returns>
+        public static int array_push(PhpArray array, params PhpValue[] vars)
+        {
+            if (array == null)
+            {
+                //PhpException.ReferenceNull("array");
+                //return 0;
+                throw new ArgumentNullException();
+            }
+
+            // adds copies variables (if called by PHP):
+            for (int i = 0; i < vars.Length; i++)
+            {
+                array.Add(vars[i]);
+            }
+
+            return array.Count;
+        }
+
+        /// <summary>
+        /// Removes the first item of an array and reindex integer keys starting from zero.
+        /// </summary>
+        /// <param name="array">The array to be shifted.</param>
+        /// <returns>The removed object.</returns>
+        /// <remarks>Resets intrinsic enumerator.</remarks>
+        public static PhpValue array_shift(PhpArray array)
+        {
+            if (array == null)
+            {
+                //PhpException.ReferenceNull("array");
+                //return null;
+                throw new ArgumentNullException();
+            }
+
+            if (array.Count == 0) return PhpValue.Null;
+
+            // dereferences result since the array doesn't do so:
+            var result = array.RemoveFirst().Value;  // TODO: PhpVariable.Dereference
+
+            // reindexes integer keys starting from zero:
+            array.ReindexIntegers(0);
+            array.RestartIntrinsicEnumerator();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Inserts specified items before the first item of an array and reindex integer keys starting from zero.
+        /// </summary>
+        /// <param name="array">The array to be unshifted.</param>
+        /// <param name="vars">Variables to be inserted.</param>
+        /// <returns>The number of items in resulting array.</returns>
+        public static int array_unshift(PhpArray array, params PhpValue[] vars)
+        {
+            if (array == null)
+            {
+                //PhpException.ReferenceNull("array");
+                //return 0;
+                throw new ArgumentNullException();
+            }
+
+            // reindexes integer keys starting from the number of items to be prepended:
+            array.ReindexIntegers(vars.Length);
+
+            // prepends items indexing keys from 0 to the number of items - 1:
+            for (int i = vars.Length - 1; i >= 0; i--)
+            {
+                array.Prepend(i, vars[i]);
+            }
+
+            return array.Count;
+        }
+
+        /// <summary>
+        /// Returns array which elements are taken from a specified one in reversed order.
+        /// Integer keys are reindexed starting from zero.
+        /// </summary>
+        /// <param name="array">The array to be reversed.</param>
+        /// <returns>The array <paramref name="array"/> with items in reversed order.</returns>
+        public static PhpArray array_reverse(PhpArray array)
+        {
+            return array_reverse(array, false);
+        }
+
+        /// <summary>
+        /// Returns array which elements are taken from a specified one in reversed order.
+        /// </summary>
+        /// <param name="array">The array to be reversed.</param>
+        /// <param name="preserveKeys">Whether keys should be left untouched. 
+        /// If set to <b>false</b> then integer keys are reindexed starting from zero.</param>
+        /// <returns>The array <paramref name="array"/> with items in reversed order.</returns>
+        public static PhpArray array_reverse(PhpArray array, bool preserveKeys)
+        {
+            if (array == null)
+            {
+                //PhpException.ReferenceNull("array");
+                //return null;
+                throw new ArgumentNullException();
+            }
+
+            PhpArray result = new PhpArray();
+
+            var e = array.GetFastEnumerator();
+
+            if (preserveKeys)
+            {
+                // changes only the order of elements:
+                while (e.MoveNext())
+                {
+                    result.Prepend(e.CurrentKey, e.CurrentValue);
+                }
+            }
+            else
+            {
+                // changes the order of elements and reindexes integer keys:
+                int i = array.IntegerCount;
+                while (e.MoveNext())
+                {
+                    var key = e.CurrentKey;
+                    result.Prepend(key.IsString ? key : new IntStringKey(--i), e.CurrentValue);
+                }
+            }
+
+            // if called by PHP languge then all items in the result should be inplace deeply copied:
+            //result.InplaceCopyOnReturn = true;
+            return result;
+        }
+
+        #endregion
     }
 }
