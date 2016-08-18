1805e41b69589353937e362bdcf136b9b855ec94
Jakub Míšek <jmisek@gmail.com>
2016-08-18 13:38:50 +0200

    sample asp.net core web server
    - peachserver

---
+++ src/Tools/peachserver/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peachserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5000/")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
