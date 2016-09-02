6cf3cac57c252ad2c27595adabb4169a614ee0b0
Jakub Míšek <jmisek@gmail.com>
2016-09-02 13:01:58 +0200

    nugets, cleanup
    - updated nuget packages
    - removed dotnet-compile-php project
    - added Peachpie.CodeAnalysis project with compiler
    - updated Peachpie.NETCore.Compilers.Tools calling compiler

--- src/Peachpie.NETCore.Compiler.Tools/Program.cs
+++ src/Peachpie.NETCore.Compiler.Tools/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Pchp.CodeAnalysis.CommandLine;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,9 +16,121 @@ namespace Peachpie.NETCore.Compiler.Tools
         /// <c>dotnet-compile-php</c> entry point.
         /// </summary>
         /// <param name="args">Arguments passed from <c>dotnet build</c>.</param>
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            throw new NotImplementedException();
+            args = ProcessArguments(args);
+
+            // compile
+            return PhpCompilerDriver.Run(PhpCommandLineParser.Default, null, args, null, Directory.GetCurrentDirectory(), null, null, new SimpleAnalyzerAssemblyLoader(), Console.Out);
         }
+
+        #region ProcessArguments
+
+        // TODO: CommonCompilerOptionsCommandLine:
+
+        static string[] ProcessArguments(string[] args)
+        {
+            var todo = new Queue<string>(args);
+            var newargs = new List<string>(args);
+
+            while (todo.Count != 0)
+            {
+                var arg = todo.Dequeue();
+                if (string.IsNullOrEmpty(arg))
+                {
+                    continue;
+                }
+
+                if (arg[0] == '@')
+                {
+                    foreach (var tmp in ProcessRsp(arg.Substring(1)))
+                    {
+                        todo.Enqueue(tmp);
+                    }
+                }
+                else
+                {
+                    var opt = ParseOption(arg);
+                    if (opt.HasValue)
+                    {
+                        var value = opt.Value.Value.ToLowerInvariant();
+
+                        switch (opt.Value.Key.ToLowerInvariant())
+                        {
+                            case "emit-entry-point":
+                                newargs.Insert(0, "/target:" + ((value == "true") ? "exe" : "library"));
+                                break;
+                            case "define": // DEBUG => /debug+
+                                if (value == "debug") newargs.Insert(0, "/debug+");
+                                break;
+                        }
+
+                        // TODO: portable PDB
+                    }
+                }
+            }
+
+            //
+            return newargs.ToArray();
+        }
+
+        static KeyValuePair<string, string>? ParseOption(string arg)
+        {
+            string value;
+            var colon = arg.IndexOf(':');
+            if (colon >= 0)
+            {
+                value = arg.Substring(colon + 1);
+                arg = arg.Remove(colon);
+            }
+            else
+            {
+                value = null;
+            }
+
+            if (arg.StartsWith("--"))
+            {
+                return new KeyValuePair<string, string>(arg.Substring(2), value);
+            }
+            else if (arg.StartsWith("-"))
+            {
+                return new KeyValuePair<string, string>(arg.Substring(1), value);
+            }
+            else
+            {
+                return null; // ignore /, \\, may be a file name, we don't have tro handle this in here
+            }
+        }
+
+        static IEnumerable<string> ProcessRsp(string fname)
+        {
+            try
+            {
+                return File.ReadAllLines(fname);
+            }
+            catch
+            {
+                return new string[0];
+            }
+        }
+
+        #endregion
+
+        #region SimpleAnalyzerAssemblyLoader (NotImplementedException)
+
+        class SimpleAnalyzerAssemblyLoader : Microsoft.CodeAnalysis.IAnalyzerAssemblyLoader
+        {
+            public void AddDependencyLocation(string fullPath)
+            {
+                throw new NotImplementedException();
+            }
+
+            public System.Reflection.Assembly LoadFromPath(string fullPath)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
     }
 }
