dd2a21fd182cfb188342e23022db4cc6d3bc8f01
Jakub Míšek <jmisek@gmail.com>
2016-09-05 17:25:39 +0200

    build script for nugets

--- src/build.ps1
+++ src/build.ps1
@@ -1,8 +1,6 @@
-dotnet pack -c Release -o . --version-suffix preview3 Peachpie.Runtime
-dotnet pack -c Release -o . --version-suffix preview3 Peachpie.NETStandard.Library
-dotnet pack -c Release -o . --version-suffix preview3 Peachpie.NETCore.Compiler.Tools
+dotnet pack -c Release -o .nugs --version-suffix preview3 Peachpie.Runtime
+dotnet pack -c Release -o .nugs --version-suffix preview3 Peachpie.NETStandard.Library
+dotnet pack -c Release -o .nugs --version-suffix preview3 Peachpie.NETCore.Compiler.Tools
 
-..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec
-..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec
-
-pause
+..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-preview3
+..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-preview3
