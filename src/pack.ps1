487e1463600a1032d93f09da502d5068e9609b3c
Jakub Míšek <jmisek@gmail.com>
2016-09-05 22:26:48 +0200

    nugets 0.1.1-preview-5
    - netcoreapp1.0

--- src/pack.ps1
+++ src/pack.ps1
@@ -1,6 +1,6 @@
-dotnet pack -c Release -o .nugs --version-suffix preview3 Peachpie.Runtime
-dotnet pack -c Release -o .nugs --version-suffix preview3 Peachpie.NETStandard.Library
-dotnet pack -c Release -o .nugs --version-suffix preview3 Peachpie.NETCore.Compiler.Tools
+dotnet pack -c Release -o .nugs --version-suffix preview5 Peachpie.Runtime
+dotnet pack -c Release -o .nugs --version-suffix preview5 Peachpie.NETStandard.Library
+dotnet pack -c Release -o .nugs --version-suffix preview5 Peachpie.NETCore.Compiler.Tools
 
-..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-preview3
-..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-preview3
+..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-preview5
+..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-preview5
