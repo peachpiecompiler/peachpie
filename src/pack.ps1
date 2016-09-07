21f5dcd6aa1438ae979910b65710ed61c29481c4
Jakub Míšek <jmisek@gmail.com>
2016-09-07 17:06:41 +0200

    nuget project references

--- src/pack.ps1
+++ src/pack.ps1
@@ -1,6 +1,8 @@
-dotnet pack -c Release -o .nugs --version-suffix preview6 Peachpie.Runtime
-dotnet pack -c Release -o .nugs --version-suffix preview6 Peachpie.NETStandard.Library
-dotnet pack -c Release -o .nugs --version-suffix preview6 Peachpie.NETCore.Compiler.Tools
+$suffix = "preview6"
 
-..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-preview6
-..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-preview6
+dotnet pack -c Release -o .nugs --version-suffix $suffix Peachpie.Runtime
+dotnet pack -c Release -o .nugs --version-suffix $suffix Peachpie.NETStandard.Library
+dotnet pack -c Release -o .nugs --version-suffix $suffix Peachpie.NETCore.Compiler.Tools
+
+..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-$suffix
+..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-$suffix
