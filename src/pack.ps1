ed5e48f656eff1cedbe580c5b43edc256c41d42a
Jakub Míšek <jmisek@gmail.com>
2016-09-07 09:22:34 +0200

    nuspec update 0.1.1-preview6
    - project dependencies fix

--- src/pack.ps1
+++ src/pack.ps1
@@ -1,6 +1,6 @@
-dotnet pack -c Release -o .nugs --version-suffix preview5 Peachpie.Runtime
-dotnet pack -c Release -o .nugs --version-suffix preview5 Peachpie.NETStandard.Library
-dotnet pack -c Release -o .nugs --version-suffix preview5 Peachpie.NETCore.Compiler.Tools
+dotnet pack -c Release -o .nugs --version-suffix preview6 Peachpie.Runtime
+dotnet pack -c Release -o .nugs --version-suffix preview6 Peachpie.NETStandard.Library
+dotnet pack -c Release -o .nugs --version-suffix preview6 Peachpie.NETCore.Compiler.Tools
 
-..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-preview5
-..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-preview5
+..\tools\nuget pack .\Peachpie.NETStandard.App.nuspec -o .nugs -version 0.1.1-preview6
+..\tools\nuget pack .\Peachpie.CodeAnalysis.nuspec -o .nugs -version 0.1.1-preview6
