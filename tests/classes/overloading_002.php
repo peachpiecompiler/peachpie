99adb2b55a2628cddd3fb0d9ec37cfbb743ce285
Jakub Míšek <jmisek@gmail.com>
2016-11-06 19:59:49 +0100

    test - overriding with ghost stub

---
+++ tests/classes/overloading_002.php
@@ -0,0 +1,24 @@
+<?php
+
+class A
+{
+	function foo() : int { return 123; } 
+}
+
+class B extends A
+{
+	function foo($a = 999) : int { return $a; }
+}
+
+function test() {
+	$a = new A;
+	echo $a->foo();
+	
+	$a = new B;
+	echo $a->foo(555);
+	echo $a->foo();
+}
+
+test();
+
+echo "Done.";
