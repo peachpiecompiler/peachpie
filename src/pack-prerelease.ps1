4d28d5750c512707c8d257a77239e2bf2535435c
Jakub Míšek <jmisek@gmail.com>
2016-11-11 14:03:29 +0100

    nugets update

--- src/pack-prerelease.ps1
+++ src/pack-prerelease.ps1
@@ -1,21 +1,14 @@
 Param(
   [string]$version = "0.2.0",
   [string]$config = "Release",
-  [string]$suffix = "preview-1"
+  [string]$suffix = "preview"
 )
 
-// TODO: compile CodeAnalysis
-
-$out = "../.nugs"
-$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.App", "Peachpie.Compiler.Tools")
-$nuspecs = @("Peachpie.Compiler.nuspec")
+$out = "../.nugs/${config}"
+$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools")
 
 foreach ($p in $projects) {
    dotnet pack -c $config -o $out --version-suffix $suffix $p
-}
-
-foreach ($n in $nuspecs) {
-   ..\tools\nuget pack $n -o $out -version "${version}-${suffix}"
 }
 
 pause
