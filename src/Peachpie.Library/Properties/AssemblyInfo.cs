d9fc55007fd0782275a0416ca1846604da7f93c9
Jakub Míšek <jmisek@gmail.com>
2016-11-21 15:10:45 +0100

    AssemblyInfo update

--- src/Peachpie.Library/Properties/AssemblyInfo.cs
+++ src/Peachpie.Library/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Peachpie.NETStandard.Library")]
+[assembly: AssemblyProduct("Peachpie.Library")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
