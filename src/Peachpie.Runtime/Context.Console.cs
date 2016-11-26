6c66442da4c7e58d254b9fc46fad193aff4d7be2
Jakub Míšek <jmisek@gmail.com>
2016-11-26 16:26:53 +0100

    globals $argv, $argc in console app

--- src/Peachpie.Runtime/Context.Console.cs
+++ src/Peachpie.Runtime/Context.Console.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,13 +17,32 @@ namespace Pchp.Core
             readonly string _rootPath;
 
             public ConsoleContext(params string[] args)
-            {   
+            {
                 _textSink = Console.Out;
                 _streamSink = Console.OpenStandardOutput();
                 _rootPath = Directory.GetCurrentDirectory() + "\\";
                 IsOutputBuffered = false;   // initializes Output
 
-                // TODO: Globals
+                // Globals
+                IntializeArgvArgc(args);
+            }
+
+            /// <summary>Initializes global $argv and $argc variables.</summary>
+            void IntializeArgvArgc(params string[] args)
+            {
+                Debug.Assert(args != null);
+
+                // command line argc, argv:
+                var argv = new PhpArray(args.Length);
+
+                // adds all arguments to the array (the 0-th argument is not '-' as in PHP but the program file):
+                for (int i = 0; i < args.Length; i++)
+                {
+                    argv.Add(i, PhpValue.Create(args[i]));
+                }
+
+                this.Globals["argv"] = PhpValue.Create(argv);
+                this.Globals["argc"] = PhpValue.Create(args.Length);
             }
 
             public override string RootPath => _rootPath;
