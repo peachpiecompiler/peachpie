8b3a26776af41b30df8f14234a171b48245de317
Robert Husák <robert@husak.cloud>
2016-11-30 15:27:07 +0100

    Create basic error reporting data types

---
+++ src/Peachpie.CodeAnalysis/Errors/ErrorType.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pchp.CodeAnalysis.Errors
+{
+    internal class ErrorType
+    {
+        private readonly string messageId;
+        private readonly string formatString;
+
+        public ErrorType(ErrorCode code)
+        {
+            this.Id = (int)code;
+            this.Severity = code.ToString().StartsWith("WRN_") ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error;
+            this.messageId = code.ToString();
+
+            Debug.Assert(ErrorStrings.ResourceManager.GetString(this.messageId) != null, $"String '{this.messageId}' could not be found in resources!");
+        }
+
+        public ErrorType(int id, DiagnosticSeverity severity, string formatString)
+        {
+            this.Id = id;
+            this.Severity = severity;
+            this.formatString = formatString;
+        }
+
+        public int Id { get; private set; }
+
+        public DiagnosticSeverity Severity { get; private set; }
+
+        public string FormatString
+        {
+            get
+            {
+                // To handle caching and possible explicit creation of this class with the error string message
+                if (this.formatString == null)
+                {
+                    Debug.Assert(this.messageId != null);
+                    ErrorStrings.ResourceManager.GetString(this.messageId);
+                }
+
+                return this.formatString;
+            }
+        }
+
+        public PhpDiagnosticInfo CreateDiagnosticInfo(object[] args) => new PhpDiagnosticInfo(this.Id, args);
+
+        public PhpDiagnosticInfo CreateDiagnosticInfo(bool isWarningAsError, object[] args) =>
+            new PhpDiagnosticInfo(isWarningAsError, this.Id, args);
+    }
+}
---
+++ src/Peachpie.CodeAnalysis/Errors/PhpDiagnosticInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace Pchp.CodeAnalysis.Errors
+{
+    internal class PhpDiagnosticInfo : DiagnosticInfo
+    {
+        internal PhpDiagnosticInfo(int errorCode, object[] args)
+            : base(Pchp.CodeAnalysis.Errors.MessageProvider.Instance, errorCode, args)
+        {
+        }
+
+        internal PhpDiagnosticInfo(bool isWarningAsError, int errorCode, object[] args)
+            : base(Pchp.CodeAnalysis.Errors.MessageProvider.Instance, isWarningAsError, errorCode, args)
+        {
+        }
+    }
+}
