af88ec31b9c62df64d5482b373d2dabead22983a
Jakub Míšek <jmisek@gmail.com>
2016-12-05 18:47:16 +0100

    targeting dev branch for dev builds to avoid NuGet restore from nuget.org

--- Samples/ConsoleApplication1/program.php
+++ Samples/ConsoleApplication1/program.php
@@ -1,3 +1,15 @@
 ﻿<?php
 
-echo "Hello World!";
+function test()
+{
+	$arr = [1, 2];
+	$arr[] = &['x'];
+	$arr[] = 3;
+	$arr[] = &$arr[2];
+
+	var_dump($arr);
+	var_export($arr);
+	print_r($arr);
+}
+
+test();
