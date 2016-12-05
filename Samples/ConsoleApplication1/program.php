97a7243c1691ddedb9ad7a40c96bca3fe95d5fb7
Jakub Míšek <jmisek@gmail.com>
2016-12-05 18:53:32 +0100

    sample fix

--- Samples/ConsoleApplication1/program.php
+++ Samples/ConsoleApplication1/program.php
@@ -1,15 +1,3 @@
 ﻿<?php
 
-function test()
-{
-	$arr = [1, 2];
-	$arr[] = &['x'];
-	$arr[] = 3;
-	$arr[] = &$arr[2];
-
-	var_dump($arr);
-	var_export($arr);
-	print_r($arr);
-}
-
-test();
+echo "Hello World!";
