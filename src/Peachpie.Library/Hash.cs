f8fe9b09aa7aa72258ebab417f6ab6c68ee4b0ca
Jakub Míšek <jmisek@gmail.com>
2017-01-04 16:07:00 +0100

    md5()

---
+++ src/Peachpie.Library/Hash.cs
@@ -0,0 +1,32 @@
+﻿using Pchp.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pchp.Library
+{
+    /// <summary>
+    /// PHP hash functions support.
+    /// </summary>
+    [PhpExtension("hash")]
+    public static class Hash
+    {
+        /// <summary>
+        /// Calculate the md5 hash of a string.
+        /// </summary>
+        /// <param name="str">Input string.</param>
+        /// <param name="raw_output">If the optional raw_output is set to TRUE, then the md5 digest is instead returned in raw binary format with a length of 16.</param>
+        /// <returns>Returns the hash as a 32-character hexadecimal number.</returns>
+        public static PhpString md5(PhpString str, bool raw_output = false)
+        {
+            var bytes = MD5.Create().ComputeHash(str.ToBytes(Encoding.UTF8));
+            return raw_output
+                ? new PhpString(bytes)
+                : new PhpString(StringUtils.BinToHex(bytes, string.Empty));
+        }
+    }
+}
