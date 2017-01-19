d5c9b1304165cffbe55adf27c9ce3bd4044231b4
Jakub Míšek <jmisek@gmail.com>
2017-01-19 19:52:51 +0100

    test - array instrict iterator

---
+++ tests/arrays/pos.php
@@ -0,0 +1,30 @@
+<?php
+
+function f($x)
+{
+  $x = (($x===null) ? "NULL" : (($x==="") ? "''" : (($x===false) ? "FALSE" : $x)));
+  return "$x\n";
+}
+
+$a = array(1,0,"",null,false,10);
+
+echo f(current($a));
+echo f(key($a));
+echo f(next($a));
+echo f(current($a));
+echo f(key($a));
+echo f(next($a));
+echo f(current($a));
+echo f(key($a));
+echo f(next($a));
+echo f(current($a));
+echo f(key($a));
+echo f(next($a));
+echo f(current($a));
+echo f(key($a));
+echo f(next($a));
+echo f(current($a));
+echo f(key($a));
+echo f(next($a));
+echo f(current($a));
+echo f(key($a));
