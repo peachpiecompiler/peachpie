adddbe8a60849f258aa0b97ed78436f05c10594c
Jakub Míšek <jmisek@gmail.com>
2017-01-21 17:27:52 +0100

    tests fix
    - avoiding notices

--- tests/classes/properties_001.php
+++ tests/classes/properties_001.php
@@ -10,12 +10,12 @@ class X {
 	static $id = 0;
 
 	function __construct () {
-		// @$this->id = 10;	// accessing static field with instance
+		@$this->id = 10;	// accessing static field with instance
 		self::$id = 10;
 
 		$a = new Y();
 		$a->fld = $this;
-		$a->fld->id = 4;
+		@$a->fld->id = 4;
 
 
 		$v = "fld";
