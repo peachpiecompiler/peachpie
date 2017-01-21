710a165e777a03b4322bbbc7b30516321b2b0c41
Jakub Míšek <jmisek@gmail.com>
2017-01-21 19:43:18 +0100

    test - object creation

---
+++ tests/classes/object_creation_test.php
@@ -0,0 +1,35 @@
+<?php
+
+class Class1
+{
+	public function __construct()
+	{
+		echo 2;
+	}
+	
+	public function Class1()
+	{
+		echo 3;
+	}
+};
+
+class Class2 extends Class1
+{
+	public function __construct()
+	{
+		echo 4;
+	}
+	
+	public function Class2()
+	{
+		echo 5;
+	}
+
+	public function Class1()
+	{
+		echo 6;
+	}
+};
+	
+echo (new Class1 instanceof Class1);
+echo (new Class2 instanceof Class1);
