3f10c804260cdd74dee8c959a808fbbacbde0b10
Jakub Míšek <jmisek@gmail.com>
2017-01-23 16:17:12 +0100

    test for chaining
    - ensuring of arrays and objects and runtime fields
    - ensuring field references

---
+++ tests/classes/chaining_test.php
@@ -0,0 +1,12 @@
+<?php
+
+class Class1
+{
+	public static $x;
+	public static $y;
+};
+
+Class1::$x->a->b->c[0][1][2]->x["A"]->y["B"]->z["C"] = "Hujer";
+Class1::$y->a->b->c[0][1][2]->x["A"]->y["B"]->z["C"] =& Class1::$x->a->b->c;
+
+echo Class1::$y->a->b->c[0][1][2]->x["A"]->y["B"]->z["C"][0][1][2]->x["A"]->y["B"]->z["C"];
