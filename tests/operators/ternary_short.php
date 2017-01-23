40bddba98bf89bf2c7ce05883c03a28122219d0b
Jakub Míšek <jmisek@gmail.com>
2017-01-23 16:20:04 +0100

    test fix - ternary_short
    - print_r instead of var_dump

---
+++ tests/operators/ternary_short.php
@@ -0,0 +1,23 @@
+<?php
+
+function foo($x)
+{
+	echo "'$x' ";
+	return $x;
+}
+
+print_r( foo(0) ?: foo(2) );
+print_r( foo(1) ?: foo(2) );
+print_r( true ?: false );
+print_r( false ?: true );
+print_r( false ?: false );
+print_r( true ?: true );
+print_r( 1 ?: 2 );
+print_r( 0 ?: 1 );
+print_r( foo(0) ?: foo(0) ?: foo(0) ?: foo(3) );
+print_r( foo(0) ?: 0 ?: foo(0) ?: foo(3) );
+print_r( foo(0) ?: foo(0) ?: foo(0) ?: 3 );
+print_r( 0 ?: 0 ?: 0 ?: foo(3) );
+print_r( 0 ?: 1 ?: foo(0) ?: foo(3) );
+
+echo "\nDONE";
