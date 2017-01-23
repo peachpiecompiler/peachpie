fef009988dcfc0bd5b3ece991e929ec93b4768b0
Jakub Míšek <jmisek@gmail.com>
2017-01-23 15:56:09 +0100

    test for ternary operator

---
+++ tests/constructs/ternary_ex.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = array(
+
+function_exists('key') ? 1:0 => function_exists('key') ? 1:0,
+function_exists('unknown') ? 1:0 => function_exists('unknown') ? 1:0
+
+);
+
+$b = array(1,2,3,4);
+
+$c = array(1=>1,2=>2);
+
+$d = $a ? 1 : 2;
+
+print_r($a);
+print_r($b);
+print_r($c);
+print_r($d);
