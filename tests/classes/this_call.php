634b7f8aa52b61d09675f1bd493dae5a66d539c7
Jakub Míšek <jmisek@gmail.com>
2017-01-23 15:59:17 +0100

    test for method dispatch

---
+++ tests/classes/this_call.php
@@ -0,0 +1,25 @@
+<?php
+
+class X {
+	
+    public function __call($name, $args) {
+        print_r($name);
+		print_r($args);        
+
+        return 123;
+    }
+
+    protected function bar2($arg) {
+        echo ("bar2".$arg);
+    }
+
+    public function bar() {
+        return
+            $this->bar2("hello",1,2,3,4,5,6,7,8,9) .
+            $this->bar2("hello") .
+            $this->bar3("hello",1,2,3,4,5,6,7,8,9);
+    }
+}
+
+$x = new X();
+echo $x->bar();
