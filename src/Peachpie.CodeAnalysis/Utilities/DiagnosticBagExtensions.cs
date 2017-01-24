75d8dc3b7ef6426a420ea06d3c4dd801bfa3b2d5
Robert Husák <robert@husak.cloud>
2017-01-24 20:44:44 +0100

    Reuse existing syntax tree in diagnostics

--- src/Peachpie.CodeAnalysis/Utilities/DiagnosticBagExtensions.cs
+++ src/Peachpie.CodeAnalysis/Utilities/DiagnosticBagExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis.Text;
 using Pchp.CodeAnalysis.Errors;
 using Pchp.CodeAnalysis.Semantics;
+using Pchp.CodeAnalysis.Symbols;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -12,10 +13,15 @@ namespace Pchp.CodeAnalysis
 {
     internal static class DiagnosticBagExtensions
     {
-        public static void Add(this DiagnosticBag diagnostics, IPhpOperation operation, ErrorCode code, params object[] args)
+        public static void Add(
+            this DiagnosticBag diagnostics,
+            SourceRoutineSymbol routine,
+            IPhpOperation operation,
+            ErrorCode code,
+            params object[] args)
         {
             // TODO: Reuse the existing one instead
-            var tree = new PhpSyntaxTree(operation.PhpSyntax.ContainingSourceUnit);
+            var tree = routine.ContainingFile.SyntaxTree;
             var span = operation.PhpSyntax.Span;
             var location = new SourceLocation(tree, new TextSpan(span.Start, span.Length));
             diagnostics.Add(location, code, args);
