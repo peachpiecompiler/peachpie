56c68090e70bea79d17b7fa6a5fc4f7c8b9a99a1
Jakub Míšek <jmisek@gmail.com>
2017-01-27 17:15:30 +0100

    tests - reflection
    - methods
    - parent classes

---
+++ tests/reflection/class_parent_001.php
@@ -0,0 +1,28 @@
+<?php
+
+class foo {}
+class bar extends foo {}
+class baz extends bar {}
+
+print_r(class_parents(new baz));
+
+class dad {
+
+}
+
+class child extends dad {
+    function __construct()
+    {
+        echo "I'm " , get_parent_class($this) , "'s son\n";
+    }
+}
+
+class child2 extends dad {
+    function __construct()
+    {
+        echo "I'm " , get_parent_class('child2') , "'s son too\n";
+    }
+}
+
+new child;
+new child2;
