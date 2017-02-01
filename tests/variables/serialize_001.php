94e9c642fc771a3311d1da0090feb6e0bc84b207
Jakub Míšek <jmisek@gmail.com>
2017-02-01 22:53:40 +0100

    tests - serialize, unserialize, print_r

---
+++ tests/variables/serialize_001.php
@@ -0,0 +1,17 @@
+<?php
+
+function f()
+{
+    $o = (object)["a" => 123, "b" => [1, 2, 3]];
+	print_r($o);
+
+	$s = serialize($o);
+	print_r($s);
+	
+	$o = unserialize($s);
+	print_r($o);
+}
+
+f();
+
+echo "Done";
