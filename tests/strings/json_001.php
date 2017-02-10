2a524f7bd87d39e806f065882449affdd163bab1
Jakub Míšek <jmisek@gmail.com>
2017-02-10 12:18:47 +0100

    removed UTF BOM from test

--- tests/strings/json_001.php
+++ tests/strings/json_001.php
@@ -1,13 +1,15 @@
-﻿<?php
+<?php
 
 function test($obj, $opt = 0)
 {
 	print_r($obj);
-	echo( $x = json_encode($obj, $opt) ), "\n";
+	$x = json_encode($obj, $opt);
+	echo "\n$x\n";
 	print_r( json_decode($x, true) );
+	echo "\n";
 }
 
-test("special characters \"/#@<>&'\"");
+test("special characters \" / # @ < > & ' \" ");
 test(array(1,2,3,4,array(1,2,'x'=>'y')));
 test(array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5));
 
@@ -28,4 +30,6 @@ echo "Empty array output as object: ", json_encode($b, JSON_FORCE_OBJECT), "\n\n
 $c = array(array(1,2,3));
 
 echo "Non-associative array output as array: ", json_encode($c), "\n";
-echo "Non-associative array output as object: ", json_encode($c, JSON_FORCE_OBJECT), "\n\n";
+echo "Non-associative array output as object: ", json_encode($c, JSON_FORCE_OBJECT);
+
+echo "Done.";
