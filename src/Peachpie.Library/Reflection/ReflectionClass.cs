c07cfbff8a09cecdab0e910dfeecdbcb914e99b1
Jakub Míšek <jmisek@gmail.com>
2017-03-17 11:28:28 +0100

    ReflectionClass stub

--- src/Peachpie.Library/Reflection/ReflectionClass.cs
+++ src/Peachpie.Library/Reflection/ReflectionClass.cs
@@ -93,6 +93,56 @@ namespace Pchp.Library.Reflection
 
         #endregion
 
+        public static string export(PhpValue argument, bool @return = false) { throw new NotImplementedException(); }
+        public PhpValue getConstant(string name) { throw new NotImplementedException(); }
+        public PhpArray getConstants() { throw new NotImplementedException(); }
+        //public ReflectionMethod getConstructor() { throw new NotImplementedException(); }
+        public PhpArray getDefaultProperties() { throw new NotImplementedException(); }
+        public string getDocComment() { throw new NotImplementedException(); }
+        public int getEndLine() { throw new NotImplementedException(); }
+        //public ReflectionExtension getExtension() { throw new NotImplementedException(); }
+        public string getExtensionName() { throw new NotImplementedException(); }
+        public string getFileName() { throw new NotImplementedException(); }
+        public PhpArray getInterfaceNames() { throw new NotImplementedException(); }
+        public PhpArray getInterfaces() { throw new NotImplementedException(); }
+        //public ReflectionMethod getMethod(string name) { throw new NotImplementedException(); }
+        public PhpArray getMethods(int filter) { throw new NotImplementedException(); }
+        public int getModifiers() { throw new NotImplementedException(); }
+        public string getName() { throw new NotImplementedException(); }
+        public string getNamespaceName() { throw new NotImplementedException(); }
+        public ReflectionClass getParentClass() { throw new NotImplementedException(); }
+        public PhpArray getProperties(int filter) { throw new NotImplementedException(); }
+        //public ReflectionProperty getProperty(string name) { throw new NotImplementedException(); }
+        public string getShortName() { throw new NotImplementedException(); }
+        public int getStartLine() { throw new NotImplementedException(); }
+        public PhpArray getStaticProperties() { throw new NotImplementedException(); }
+        public PhpValue getStaticPropertyValue(string name) { throw new NotImplementedException(); }
+        public PhpValue getStaticPropertyValue(string name, PhpAlias def_value) { throw new NotImplementedException(); }
+        public PhpArray getTraitAliases() { throw new NotImplementedException(); }
+        public PhpArray getTraitNames() { throw new NotImplementedException(); }
+        public PhpArray getTraits() { throw new NotImplementedException(); }
+        public bool hasConstant(string name) { throw new NotImplementedException(); }
+        public bool hasMethod(string name) { throw new NotImplementedException(); }
+        public bool hasProperty(string name) { throw new NotImplementedException(); }
+        public bool implementsInterface(string @interface) { throw new NotImplementedException(); }
+        public bool inNamespace() { throw new NotImplementedException(); }
+        public bool isAbstract() { throw new NotImplementedException(); }
+        public bool isAnonymous() { throw new NotImplementedException(); }
+        public bool isCloneable() { throw new NotImplementedException(); }
+        public bool isFinal() { throw new NotImplementedException(); }
+        public bool isInstance(object @object) { throw new NotImplementedException(); }
+        public bool isInstantiable() { throw new NotImplementedException(); }
+        public bool isInterface() { throw new NotImplementedException(); }
+        public bool isInternal() { throw new NotImplementedException(); }
+        public bool isIterateable() { throw new NotImplementedException(); }
+        public bool isSubclassOf(string @class) { throw new NotImplementedException(); }
+        public bool isTrait() { throw new NotImplementedException(); }
+        public bool isUserDefined() { throw new NotImplementedException(); }
+        public object newInstance(params PhpValue[] args) { throw new NotImplementedException(); }
+        public object newInstanceArgs(PhpArray args) { throw new NotImplementedException(); }
+        public object newInstanceWithoutConstructor() { throw new NotImplementedException(); }
+        public void setStaticPropertyValue(string name, PhpValue value) { throw new NotImplementedException(); }
+
         #region Reflector
 
         public string __toString()
