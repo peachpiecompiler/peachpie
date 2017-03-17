4d3df95870e99c953aa3c2fe9ae070f98e996446
Jakub Míšek <jmisek@gmail.com>
2017-03-17 11:05:14 +0100

    ReflectionException

---
+++ src/Peachpie.Library/Reflection/ReflectionException.cs
@@ -0,0 +1,18 @@
+﻿using Pchp.Core;
+using Pchp.Library.Spl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pchp.Library.Reflection
+{
+    [PhpType("[name]"), PhpExtension(ReflectionUtils.ExtensionName)]
+    public class ReflectionException : Spl.Exception
+    {
+        public ReflectionException(string message = "", long code = 0, Throwable previous = null)
+            :base(message, code, previous)
+        {
+        }
+    }
+}
