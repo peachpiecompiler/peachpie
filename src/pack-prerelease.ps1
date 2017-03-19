9c7244181e7b89945bd4c8e19023bcb8f40dc0ee
Jakub Míšek <jmisek@gmail.com>
2017-03-19 16:35:30 +0100

    mssql added to build script

--- src/pack-prerelease.ps1
+++ src/pack-prerelease.ps1
@@ -4,7 +4,7 @@ Param(
 )
 
 $out = "../.nugs/${config}"
-$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.Library.MySql", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools")
+$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.Library.MySql", "Peachpie.Library.MsSql", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools")
 
 foreach ($p in $projects) {
    dotnet pack -c $config -o $out --version-suffix $suffix $p
