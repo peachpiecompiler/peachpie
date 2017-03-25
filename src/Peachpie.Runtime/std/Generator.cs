43aecadde36fd179b2e67649858f33d6d7dd76b5
Petr Houška <houskape@gmail.com>
2017-03-25 17:14:59 +0100

    Initial Generator's IDisposable implementation.

--- src/Peachpie.Runtime/std/Generator.cs
+++ src/Peachpie.Runtime/std/Generator.cs
@@ -6,7 +6,7 @@ namespace Pchp.Core.std
     internal delegate void GeneratorStateMachine(Context ctx, Generator gen);
 
     [PhpType("Generator")]
-    public class Generator : Iterator
+    public class Generator : Iterator, IDisposable
     {
         #region InternalVariables
         /// <summary>
@@ -192,6 +192,42 @@ namespace Pchp.Core.std
         private void checkIfRunToFirstYieldIfNotRun()
         {
             if(!_runToFirstYield) { _runToFirstYield = true; this.next(); }           
+        }
+
+        #endregion
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Generator() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
         }
         #endregion
 
