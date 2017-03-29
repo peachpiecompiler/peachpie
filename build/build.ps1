625c3cdd62fc94f6b8f4f18c72c023211aa09a3e
Jakub Míšek <jmisek@gmail.com>
2017-03-30 00:32:07 +0200

    solution update
    - sdk moved to src folder
    - properties updated so sdk builds fine
    - build scripts

--- build/build.ps1
+++ build/build.ps1
@@ -3,8 +3,9 @@ Param(
   [string]$suffix = "preview-" + [datetime]::now.tostring("yyyyMMdd-HHmmss")
 )
 
-$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.Library.MySql", "Peachpie.Library.MsSql", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools")
+$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.Library.MySql", "Peachpie.Library.MsSql", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools", "Peachpie.NET.Sdk")
 
 foreach ($p in $projects) {
-   msbuild.exe "..\src\$p\$p.csproj" /t:build /v:m /p:VersionSuffix=$suffix /p:Configuration=$config /p:GeneratePackageOnBuild=true
+   msbuild.exe "..\src\$p\$p.csproj" /t:restore /v:m /p:VersionSuffix=$suffix /p:PackageVersionSuffix=$suffix /p:Configuration=$config
+   msbuild.exe "..\src\$p\$p.csproj" /t:build /v:m /p:VersionSuffix=$suffix /p:PackageVersionSuffix=$suffix /p:Configuration=$config /p:GeneratePackageOnBuild=true
 }
