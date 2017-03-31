220ae4bb17c08010cbfe96f4b9d4b62a681632a5
Jakub Míšek <jmisek@gmail.com>
2017-03-31 11:26:52 +0200

    Peachpie.Library.Scripting in build script

--- build/build.ps1
+++ build/build.ps1
@@ -3,7 +3,7 @@ Param(
   [string]$suffix = "preview-" + [datetime]::now.tostring("yyyyMMdd-HHmmss")
 )
 
-$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.Library.MySql", "Peachpie.Library.MsSql", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools", "Peachpie.NET.Sdk")
+$projects = @("Peachpie.Runtime", "Peachpie.Library", "Peachpie.Library.Scripting", "Peachpie.Library.MySql", "Peachpie.Library.MsSql", "Peachpie.App", "Peachpie.CodeAnalysis", "Peachpie.NETCore.Web", "Peachpie.Compiler.Tools", "Peachpie.NET.Sdk")
 
 foreach ($p in $projects) {
    msbuild.exe "..\src\$p\$p.csproj" /t:restore /v:m /p:VersionSuffix=$suffix /p:PackageVersionSuffix=$suffix /p:Configuration=$config
