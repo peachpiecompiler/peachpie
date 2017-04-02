83bd68fa02e20d254c1e3e8422a3c9c6d56c2fa6
Jakub Míšek <jmisek@gmail.com>
2017-04-02 20:35:16 +0200

    updated build
    - compiler tools was not restored for some reason

--- build/build.ps1
+++ build/build.ps1
@@ -8,4 +8,5 @@ $props = "/p:VersionSuffix=$suffix,PackageVersionSuffix=$suffix,Configuration=$c
 Write-Host -f Green "msbuild $props ..."
 
 msbuild /t:restore "$PSScriptRoot/../Peachpie.sln" /v:m $props
+dotnet restore "$PSScriptRoot/../src/Peachpie.Compiler.Tools" /v:m $props
 msbuild /t:build "$PSScriptRoot/../Peachpie.sln" /v:m $props
