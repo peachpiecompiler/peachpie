a4c50c2eaaccc155ec758912210c0d9b7fe32879
Jakub Míšek <jmisek@gmail.com>
2017-04-06 11:25:16 +0200

    MyGet.ps1 diagnostics
    - script fails if tests fail

--- MyGet.ps1
+++ MyGet.ps1
@@ -1,2 +1,22 @@
+# Diagnostic 
+function Write-Diagnostic {
+    param([string]$message)
+
+    Write-Host
+    Write-Host -f green $message
+    Write-Host
+}
+
+function Die([string]$message, [object[]]$output) {
+    if ($output) { Write-Output $output }
+    Write-Error $message
+    exit 1
+}
+
 .\build\build.ps1 -suffix "CI$env:BuildCounter"
+if($LASTEXITCODE -ne 0) { Die("Build failed.") }
+
 dotnet test .\src\Tests\Peachpie.ScriptTests\Peachpie.ScriptTests.csproj
+if($LASTEXITCODE -ne 0) { Die("Peachpie.ScriptTests failed.") }
+
+Write-Diagnostic "Succeeded."
