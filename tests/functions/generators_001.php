64a0f1b29d5167e9df8c123dbe1a84d8094a8b74
Petr Houška <houskape@gmail.com>
2017-04-09 14:27:37 +0200

    Added generators test.
    - tests simple enumeration & explicit returns.

---
+++ tests/functions/generators_001.php
@@ -0,0 +1,73 @@
+<?php
+
+function f()
+{
+    echo "preY\n";
+	yield "a";
+    yield 5 => 2;
+    yield "7" => "c";
+    yield;
+    yield "d";
+    echo "posY\n";
+}
+
+$gen = f();
+foreach($gen as $key => $value){
+    echo "k:".$key."v:".$value."\n";
+}
+
+echo "r:".$gen->getReturn()."\n";
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
+
+echo "-------------------------------------------------\n";
+
+function h()
+{
+    echo "preY\n";
+	yield "a";
+    yield;
+    yield "b";
+    echo "posY\n";
+
+    return 2;
+}
+
+$gen = h();
+echo "1\n";
+
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
+echo "2\n";
+
+$gen->next();
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
+echo "3\n";
+
+$gen->next();
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
+echo "4\n";
+
+$gen->next();
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
+echo "r:".$gen->getReturn()."\n";
+echo "5\n";
+
+$gen->next();
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
+echo "r:".$gen->getReturn()."\n";
+echo "6\n";
+
+echo "-------------------------------------------------\n";
+
+function g()
+{
+	yield 1;
+    return "a";
+}
+
+$gen = g();
+foreach($gen as $key => $value){
+    echo "k:".$key."v:".$value."\n";
+}
+
+echo "r:".$gen->getReturn()."\n";
+echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
