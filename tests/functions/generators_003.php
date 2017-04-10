7dce6dbbc96386925f2d1b096228595846920cce
Petr Houška <houskape@gmail.com>
2017-04-10 19:58:30 +0200

    Added tests for using global vars from generators.

--- tests/functions/generators_003.php
+++ tests/functions/generators_003.php
@@ -18,7 +18,25 @@ foreach($gen as $key => $value){
 echo "r:".$gen->getReturn()."\n";
 echo "k:".$gen->key()."v:".$gen->current()."c:".$gen->valid()."\n";
 
+echo "-------------------------------------------------\n";
 
+$a = 1;
+$b = 2;
+
+function g()
+{
+    global $a, $b;
+    yield $a + $b;
+    $a = 5;
+}
+
+echo "a".$a."\n";
+foreach(g() as $key => $value){
+    echo "k:".$key."v:".$value."\n";
+}
+echo "a".$a."\n";
+
+echo "-------------------------------------------------\n";
 
 
 
