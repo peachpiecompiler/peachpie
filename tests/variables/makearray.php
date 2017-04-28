c802561f67060f43a1bd50c9af5e523fbc348b3a
Jakub Míšek <jmisek@gmail.com>
2017-04-28 19:14:02 +0200

    test for "$x[] =" array analysis

---
+++ tests/variables/makearray.php
@@ -0,0 +1,20 @@
+<?php
+
+/** The test ensures the type analysis correctly handles that Boolean type may change to Array */
+function foo($arr) {
+	$b = false;
+	
+	print_r($b);
+	
+	foreach ($arr as $x)
+	{
+		$b[] = $x;
+	}
+	
+	print_r($b);
+}
+
+foo([]);
+foo([1,2,3]);
+
+echo "Done.";
