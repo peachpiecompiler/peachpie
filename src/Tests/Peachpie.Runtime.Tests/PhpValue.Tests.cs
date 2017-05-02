070270f1b2b640a8293a957ca0d77a1c6f8f9405
Jakub Míšek <jmisek@gmail.com>
2017-05-02 15:40:39 +0200

    test - ToClr, FromClr

--- src/Tests/Peachpie.Runtime.Tests/PhpValue.Tests.cs
+++ src/Tests/Peachpie.Runtime.Tests/PhpValue.Tests.cs
@@ -12,5 +12,18 @@ namespace Peachpie.Runtime.Tests
         {
             Assert.AreEqual(Pchp.Core.Convert.StringToLongInteger("1"), 1);
         }
+
+        [TestMethod]
+        public void ClrConversion()
+        {
+            // CLR -> Value -> CLR
+            var objects = new object[] { 0L, 1L, true, 1.2, new object(), "Hello", new PhpArray() };
+            var values = PhpValue.FromClr(objects);
+            Assert.AreEqual(objects.Length, values.Length);
+            for (int i = 0; i < objects.Length; i++)
+            {
+                Assert.AreEqual(objects[i], values[i].ToClr());
+            }
+        }
     }
 }
