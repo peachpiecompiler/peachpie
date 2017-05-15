55b7baf065d3596ecbeb7ea093ad3ac1cecbdc3d
Petr Houška <houskape@gmail.com>
2017-05-15 21:48:08 +0200

    Add helper method BoundExpr.IsSafeToEvalMultipleTimes.
    - used for determining whether a temp variable in necessary or not.

---
+++ src/Peachpie.CodeAnalysis/Utilities/PhpOperationExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Pchp.CodeAnalysis.Semantics
+{
+    public static class PhpOperationExtensions
+    {
+        #region ExpressionExtensions
+        /// <summary>
+        /// Returns whether the expression is safe to be evaluated multiple times i.e it has no side effects.
+        /// </summary>
+        public static bool IsSafeToEvalMultipleTimes(this BoundExpression expr)
+        {
+            return expr is BoundReferenceExpression || expr.ConstantValue.HasValue;
+        }
+        #endregion    
+    }
+}
