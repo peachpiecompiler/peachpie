5ddd08767e2a1733814342e24b8d349f6c6f8bed
Robert Husák <robert@husak.cloud>
2017-06-20 12:38:25 +0200

    Add test for redefined field accessibility change

---
+++ tests/classes/skip(accessibility_change_in_redefined_field_not_supported)_field_inheritance_002.php
@@ -0,0 +1,39 @@
+<?php
+
+class A {
+  protected $x = -1;
+  
+  public function Aget() {
+    return $this->x;
+  }
+  
+  public function Aset($v) {
+    $this->x = $v;
+  }
+}
+
+class B extends A {
+  public $x;
+  
+  public function Bget() {
+    return $this->x;
+  }
+  
+  public function Bset($v) {
+    $this->x = $v;
+  }
+}
+
+$b = new B;
+$b->x = 123;
+echo $b->Aget() ."\n";
+
+$b = new B;
+$b->Bset(123);
+echo $b->Aget() ."\n";
+echo $b->Bget() ."\n";
+
+$b = new B;
+$b->Aset(123);
+echo $b->Aget() ."\n";
+echo $b->Bget() ."\n";
