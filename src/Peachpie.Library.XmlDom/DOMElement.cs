9ef9c65ac52940103cec1335703703b54d844d0e
Robert Husák <robert@husak.cloud>
2017-06-30 15:55:18 +0200

    Add stubs of several DOM classes

---
+++ src/Peachpie.Library.XmlDom/DOMElement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pchp.Core;
+
+namespace Peachpie.Library.XmlDom
+{
+    [PhpType(PhpTypeAttribute.InheritName)]
+    public class DOMElement
+    {
+    }
+}
