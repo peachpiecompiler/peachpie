3244aecb1bf23764d1d3ae9931c02b3903192e50
Jakub Míšek <jmisek@gmail.com>
2017-07-11 13:59:04 +0200

    not implemented conversion message

--- src/Peachpie.CodeAnalysis/CodeGen/CodeGenerator.Convert.cs
+++ src/Peachpie.CodeAnalysis/CodeGen/CodeGenerator.Convert.cs
@@ -931,7 +931,7 @@ namespace Pchp.CodeAnalysis.CodeGen
                     }
                     else
                     {
-                        throw new ArgumentException($"{from.Name} cannot be converted to a class of type {to.Name}!");  // TODO: ErrCode
+                        break;
                     }
                 default:
                     if (from == CoreTypes.PhpValue)
@@ -984,8 +984,13 @@ namespace Pchp.CodeAnalysis.CodeGen
                         EmitCastClass(from, to);
                         return;
                     }
-                    throw new NotImplementedException();
+
+                    //
+                    break;
             }
+
+            //
+            throw new NotImplementedException($"Conversion from {from.Name} to {to.Name} is not implemented.");
         }
 
         public void EmitConvert(BoundExpression expr, TypeSymbol to)
