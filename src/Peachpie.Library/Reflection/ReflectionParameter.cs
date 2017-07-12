45d6b2f7835962a24d688efca81eb1dfec1398fe
Robert Husák <robert@husak.cloud>
2017-07-12 12:25:06 +0200

    Add ReflectionParameter class stub

---
+++ src/Peachpie.Library/Reflection/ReflectionParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pchp.Core;
+
+namespace Pchp.Library.Reflection
+{
+    [PhpType("[name]"), PhpExtension(ReflectionUtils.ExtensionName)]
+    public class ReflectionParameter : Reflector
+    {
+        public string __toString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
