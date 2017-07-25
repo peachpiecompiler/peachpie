68064cf070e6e0fd838422f2a0cad1e7d2fcb9ca
Jakub Míšek <jmisek@gmail.com>
2017-07-25 21:34:24 +0200

    json_last_error_msg

--- src/Peachpie.Library/Serialization.Json.cs
+++ src/Peachpie.Library/Serialization.Json.cs
@@ -528,7 +528,7 @@ namespace Pchp.Library
         /// <summary>
         /// Occurs with underflow or with the modes mismatch.
         /// </summary>
-        public const int PHP_JSON_ERROR_STATE_MISMATCH = 2;
+        public const int JSON_ERROR_STATE_MISMATCH = 2;
 
         /// <summary>
         /// Control character error, possibly incorrectly encoded  	 
@@ -649,6 +649,23 @@ namespace Pchp.Library
         }
 
         public static int json_last_error(Context ctx) => PhpSerialization.GetLastJsonError(ctx);
+
+        /// <summary>
+        /// Returns the error string of the last <see cref="json_encode"/> or <see cref="json_decode"/> call.
+        /// </summary>
+        public static string json_last_error_msg(Context ctx)
+        {
+            switch (PhpSerialization.GetLastJsonError(ctx))
+            {
+                case JSON_ERROR_NONE: return "No error";
+                case JSON_ERROR_DEPTH: return "Maximum stack depth exceeded";
+                case JSON_ERROR_STATE_MISMATCH: return "State mismatch (invalid or malformed JSON)";
+                case JSON_ERROR_CTRL_CHAR: return "Control character error, possibly incorrectly encoded";
+                case JSON_ERROR_SYNTAX: return "Syntax error";
+                case JSON_ERROR_UTF8: return "Malformed UTF-8 characters, possibly incorrectly encoded";
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
 
         #endregion
     }
