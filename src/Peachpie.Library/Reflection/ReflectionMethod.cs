a783c97332b6dc8017f36443372c7ce4e3412524
Robert Husák <robert@husak.cloud>
2017-07-26 12:54:42 +0200

    Implement ReflectionMethod.isConstructor

--- src/Peachpie.Library/Reflection/ReflectionMethod.cs
+++ src/Peachpie.Library/Reflection/ReflectionMethod.cs
@@ -109,7 +109,7 @@ namespace Pchp.Library.Reflection
         public PhpValue invoke(Context ctx, object @object, params PhpValue[] args) => _routine.Invoke(ctx, @object, args);
         public PhpValue invokeArgs(Context ctx, object @object, PhpArray args) => _routine.Invoke(ctx, @object, args.GetValues());
         public bool isAbstract() => _routine.Methods.Any(m => m.IsAbstract);
-        public bool isConstructor() { throw new NotImplementedException(); }
+        public bool isConstructor() => name == Pchp.Core.Reflection.ReflectionUtils.PhpConstructorName;
         public bool isDestructor() { throw new NotImplementedException(); }
         public bool isFinal() => _routine.Methods.All(m => m.IsFinal);
         public bool isPrivate() { throw new NotImplementedException(); }
