b33a871bfa621034039beb858afe834848a2afa9
Jakub Míšek <jmisek@gmail.com>
2017-07-27 11:08:32 +0200

    test for const

---
+++ tests/constants/constants_002.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace
+{
+    const C1 = 12;
+    echo C1;
+}
+
+namespace A
+{
+    echo C1;    // global constant, lookups both /A/C1 and /C1
+    const C1 = 34;
+    const C2 = 56;
+    echo C1;
+    echo C2;
+
+    echo "\nE_ERROR:", E_ERROR, "\n";   // global const
+
+    echo "Done.";
+}
