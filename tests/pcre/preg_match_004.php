c5a7fb537e322adc86db51d8e0a7ce0431dbb323
Jakub Míšek <jmisek@gmail.com>
2017-08-09 13:41:24 +0200

    preg match newlines
    - matching newline characters correctly

---
+++ tests/pcre/preg_match_004.php
@@ -0,0 +1,14 @@
+<?php
+
+echo preg_match("~^\S~", "\nA") ? 1 : 0;
+echo preg_match("~^\s~", "\nA") ? 1 : 0;
+echo preg_match("~^\n~", "\nA") ? 1 : 0;
+
+echo "\n";
+
+echo preg_match("~^.~s", "\n") ? 1 : 0;
+echo preg_match("~^.~", "\n") ? 1 : 0;
+
+echo "\n";
+
+echo "Done";
