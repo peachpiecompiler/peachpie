09468d6cf263be054b9c1651ff4b9d3c642fe2b7
Robert Husák <robert@husak.cloud>
2017-08-17 14:22:28 +0200

    Add DOMXPath

---
+++ tests/xml/xpath_003.php
@@ -0,0 +1,274 @@
+<?php
+$html_text = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<HEAD>
+<TITLE>Durango DNX408 1/8 Off-road Buggy Kit for .21 Nitro engines, TDR102002</TITLE>
+<meta http-equiv="Content-type" content="text/html;charset=iso-8859-1">
+<meta name="description" content="Durango DNX408 1/8 Off-road Buggy Kit for .21 Nitro engines, TDR102002">
+<meta name="keywords" content="TDR102002 Durango DNX408 1/8 Off-road Buggy Kit for .21 Nitro engines">
+<LINK REL="stylesheet" TYPE="text/css" HREF="/styles/srh.css">
+</HEAD>
+<BODY bgcolor="#ffffff" text="#000000">
+
+
+
+
+<table width="100%"><tr><td><center><table width="95%"><tr><td>
+
+<center>
+<table class="bannerback">
+	<tr>
+		<td><a href="http://www.stormerhobbies.com/"><img src="../banners/ranban.jpg" alt="Stormer Hobbies.com" width=985 height=100 border=0></a></td>
+		<td><table>
+			<tr><td><a href="https://twitter.com/#!/stormerhobbies"><img src="../banners/twi.jpg" alt="twitter"></a></td></tr>
+			<tr><td><a href="http://www.facebook.com/StormerHobbies"><img src="../banners/fbi.jpg" alt="twitter"></a></td></tr>
+		</table></td>
+	</tr>
+</table>
+</center>
+
+
+<!--<center>
+<table class="bannerback" width="100%">
+	<tr>
+		<td align="center"><a href="http://www.stormerhobbies.com/"><img src="../banners/ranban.jpg" alt="Stormer Hobbies.com" width=985 height=100 border=0></a></td>
+	</tr>
+</table>
+</center>
+-->
+
+<!--
+<center><table><tr><td>
+</td>
+<td>
+ <table>
+  <tr>
+    <td width="15" align="right"><img border="0" src="../images/padlock.gif" alt="SSL Security" width="12" height="14"></td>
+    <td align="center">This site features Smart Shopping Carts and state of the art Secure Servers.</td>
+    <td width="15"><img border="0" src="../images/padlock.gif" alt="SSL Security" width="12" height="14"></td>
+  </tr>
+ </table>
+</td>
+<td>
+</td>
+</tr>
+</table></center>
+-->
+<hr>
+<center><a
+href="http://www.stormerhobbies.com">Storefront</a>&nbsp;|&nbsp;<a
+href="http://www.stormerhobbies.com/cgi-bin/searchpn.pl?sthumb=on">Search/Shop</a>&nbsp;|&nbsp;<a
+href="http://www.stormerhobbies.com/site_order_info.shtml">Contact&nbsp;Us</a>&nbsp;|&nbsp;<a
+href="http://www.stormerhobbies.com/site_faqs.shtml">FAQ\'s</a>&nbsp;|&nbsp;<a
+href="https://www.ssl-stormerhobbies.com/cgi-bin/login.pl">My&nbsp;Account</a>&nbsp;|&nbsp;<a
+href="https://www.ssl-stormerhobbies.com/cgi-bin/cart.pl?sub=View Cart">Shopping&nbsp;Cart</a>
+</center>
+<hr>
+<p>
+
+
+<table><tr>
+<td valign="top" width=200>
+<b>Part Number: <font color="#0000ff">TDR102002</font></b><br>
+<b>Manufacturer: Durango</b><br>
+<font size="+1"><b>Price:</b></font><br>
+$549.99 - <i>Temporarily out of stock, on order</i><br>
+
+<p>
+
+DNX408 1/8 Off-road Buggy Kit for .21 Nitro engines
+<FORM METHOD="POST" ACTION="https://www.ssl-stormerhobbies.com/cgi-bin/cart.pl">
+	<table border bgcolor="#dddddd">
+		<tr>
+			<td>
+				<center>
+					<table>
+						<tr>
+							<td align=center>
+								<font size="+1" color="#000088"><b>What would you like to do?</b></font><br>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<table width="100%"border bgcolor="#aaaaaa">
+									<tr>
+										<td align=center valign=top>
+											<INPUT TYPE="hidden" NAME="pn" VALUE="TDR102002">
+											<INPUT TYPE="hidden" NAME="src" VALUE="ns">
+											<INPUT TYPE="submit" NAME="sub" VALUE="View Cart">
+										</td>
+										<td align=center valign=top>
+											<INPUT TYPE="submit" NAME="sub" VALUE="Add to Cart"><br>
+											 Quantity: <INPUT TYPE="text" NAME="quantity" VALUE="1" SIZE="4">
+										</td>
+										<td align=center valign=top>
+											<INPUT TYPE="submit" NAME="sub" VALUE="Check Out">
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td align=center><font color="#000088" size="+1"><i>Temporarily out of stock, on order</i></font></td>
+						</tr>
+					</table>
+				</center>
+			</td>
+		</tr>
+	</table>
+</FORM>
+
+</td>
+<td>&nbsp;</td>
+<td>
+<center><img src="/pics/tdr102002.jpg" alt="TDR102002 image" title="TDR102002 image"></center>
+</td>
+</tr></table>
+&nbsp;<br>
+
+<p>
+
+
+
+
+
+<font size="+1"><b>Extended Product Information:</b></font><br>
+As you would expect from Team Durango, precision, quality and performance are the hallmarks of the DNX408. 1/8 scale nitro buggy racing makes the toughest demands of any RC car racing class,
+so Team Durango have designed a car that is as strong as it is fast!
+<p>
+The 5mm thick hard anodized chassis is one of the narrowest, if not the narrowest on the market. Not only is the chassis hard anodized, it has been machined to minimize weight and give the
+DNX408 class-leading center of gravity, with almost all the hardware sunk down into it.
+<p>
+Hard anodized alloy bulkheads front and rear keep the weight low and the car strong. Gearbox cases front and rear are molded and are separate from the load-bearing bulkheads. This gives
+minimal flex, the best bearing alignment, dirt shielding and super-easy removal of whole differential and CVD, just like the DEX410.
+<p>
+PTFE coated ultra-smooth 15mm big-bore threaded alloy shocks feature alloy caps, an alloy cartridge bottom and polished shock shafts. Mounted on 5mm hard anodized shock towers, the shock
+absorbers on the DNX408 are as smooth as possible giving you the best \'feel\' of your car as it soaks-up the roughest of tracks.
+<p>
+The single-piece CNC machined engine mount with gold anodized memory plate is designed for maximum engine performance and easy maintenance with a marked-up chassis for perfect-every-time
+engine alignment.
+<p>
+In-board and out-board CVD joints front and rear, with rubber CVD boot protection, minimize debris problems in the race and maintenance between meetings.
+<p>
+The fuel tank inlet complete with dirt guard - is center-mounted for easy pit-lane access.
+<p>
+The DEX408 has the lowest bodyshell in its class as this has many benefits, like lowering the center of gravity even more; and the minimal frontal area punches through the air allowing more
+air flow over the rear wing; it also helps with heat management.
+<p>
+The engine head in a DNX408 gets the most direct clean airflow of any car. The bodyshell is designed to direct as much air as possible over the bodyshell at the engine head, rather than
+through the body. The central engine and low bodyshell mean that all shock bodies are also in clean airflow.
+<p>
+The DNX408\'s body is long and slim at the rear which encloses the exhaust header and insulates the left rear shock, while it is trimmed to help direct/suck hot air away from either rear
+shock.
+<p>
+The super-low body ensures maximum flow over the adjustable rear wing, while the wing sits low to again lower the CG. The over-sized end-plates (ROAR legal) not only ensure all-round
+stability, they help to protect the engine head in crashes.
+<p>
+The forward-mounted engine (which is 20mm further forward than some of our competitors\' race buggies) gives ideal weight distribution and room for everything to be mounted as low as possible
+in the super-strong yet incredibly lightweight chassis.
+<p>
+The steering is set at the same up-angle as the kickup to remove bump steer, while the Ackerman link moves under the front CVD.
+<p>
+The rear wing is as close to the rear wheels as possible, thanks to the forward shock position. This drives the down-force directly to the rear wheels. Too many wings on other 1/8 buggies are
+way out back which means more wheelies than down-force.
+<p>
+<b>Kit includes:</b><br>
+Full Rolling chassis with 3 shoe clutch set, clear body, black wing, and white dish rims.
+<p>
+<b>Kit Requires:</b><br>
+<li>Radio system with 2 high torque servos<br>
+<li>.21 size nitro race engine<br>
+<li>Tuned pipe and manifold to fit engine<br>
+<li>Tires<br>
+<li>Flat Receiver battery pack<br>
+<li>Glow igniter<br>
+<li>Fuel<br>
+<li>Misc. tools for assembly and paint for body<br>
+<p>
+<img src="https://www.ssl-stormerhobbies.com/pics/tdr102002_2.jpg">
+<p>
+<img src="https://www.ssl-stormerhobbies.com/pics/tdr102002_3.jpg">
+<p>
+<img src="https://www.ssl-stormerhobbies.com/pics/tdr102002_4.jpg">
+<p>
+<img src="https://www.ssl-stormerhobbies.com/pics/tdr102002_5.jpg">
+<p>
+<img src="https://www.ssl-stormerhobbies.com/pics/tdr102002_6.jpg">
+<p>
+
+
+
+
+<big></big><br>
+
+<p>
+
+
+
+<center>
+
+</center>
+<p>
+
+
+
+
+
+<center>
+<hr width="100%">
+<table width="100%">
+<tr>
+  <td class="linkbar" align="center"><a
+	href="http://www.stormerhobbies.com/cgi-bin/searchpn.pl?sthumb=on">Search/Shop</a>&nbsp;|&nbsp;<!--<a
+	href="http://www.stormerhobbies.com/links.shtml">Links</a>&nbsp;|&nbsp;--><a
+	href="http://www.stormerhobbies.com/site_customer_service.shtml">Customer&nbsp;Service</a>&nbsp;|&nbsp;<a
+	href="http://www.stormerhobbies.com/site_order_info.shtml">Contact&nbsp;Us</a>&nbsp;|&nbsp;<a
+	href="http://www.stormerhobbies.com/site_about_us.shtml">About&nbsp;us</a>&nbsp;|&nbsp;<a
+	href="http://www.stormerhobbies.com/site_faqs.shtml">FAQ\'s</a>&nbsp;|&nbsp;<a
+	href="https://www.ssl-stormerhobbies.com/cgi-bin/login.pl">My&nbsp;Account</a>&nbsp;|&nbsp;<a
+	href="https://www.ssl-stormerhobbies.com/cgi-bin/cart.pl?sub=View Cart">Shopping&nbsp;Cart</a>&nbsp;|&nbsp;<a
+	href="http://www.stormerhobbies.com/site_privacy.shtml">Privacy&nbsp;Statement</a>&nbsp;|&nbsp;<a
+	href="http://www.stormerhobbies.com/site_terms.shtml">Terms&nbsp;of&nbsp;Use</a></td>
+</tr>
+</table>
+
+<!--
+<a
+href="http://www.stormerhobbies.com/">Storefront</a>&nbsp;|&nbsp;<a
+href="http://www.stormerhobbies.com/cgi-bin/searchpn.pl?sthumb=on">Search/Shop</a>&nbsp;|&nbsp;<a
+href="http://www.stormerhobbies.com/site_order_info.shtml">Contact&nbsp;Us</a>&nbsp;|&nbsp;<a
+href="http://www.stormerhobbies.com/site_faqs.shtml">FAQ\'s</a>&nbsp;|&nbsp;<a
+href="https://www.ssl-stormerhobbies.com/cgi-bin/login.pl">My&nbsp;Account</a>&nbsp;|&nbsp;<a
+href="https://www.ssl-stormerhobbies.com/cgi-bin/cart.pl?sub=View Cart">Shopping&nbsp;Cart</a>
+-->
+
+<table border="0" cellspacing="1" width="100%">
+<tr>
+  <td width="100%" bgcolor="#888888" align=center>
+	<font size="-1">Copyright &copy;2000-2012 <b>Stormer Hobbies, Inc.</b> All Rights Reserved</font>
+  </td>
+</tr>
+</table>
+</center>
+
+
+
+</td></tr></table>
+</center>
+</td></tr></table>
+</BODY>
+</HTML>';
+
+$html = new DOMDocument();
+
+// Parse the HTML into an XML document. This parser is reasonably tolerant.
+$html->loadHtml(str_replace('&nbsp;', ' ', $html_text));
+
+// get a new XPath object rooted at the top of the HTML DOM
+$xpath = new DOMXPath($html);
+
+if (($content = $xpath->query('//b[starts-with(., "Part Number:")]/font|//font[b[.="Price:"]]/following-sibling::text()[1]|//td[font/b[.="Price:"]]/b[3]')) === false || $content->length < 2) {
+  echo 'Table had unexpected format';
+}
+echo strtoupper(trim(html_entity_decode($content->item(0)->nodeValue))) . ':' .
+trim(html_entity_decode($content->item(1)->nodeValue));
