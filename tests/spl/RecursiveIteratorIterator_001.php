8141ac158210a0d25bab116159fffe422e093d8f
Jakub Míšek <jmisek@gmail.com>
2017-08-21 20:32:14 +0200

    RecursiveIteratorIterator tests

---
+++ tests/spl/RecursiveIteratorIterator_001.php
@@ -0,0 +1,11 @@
+<?php
+$tree = [ [ ["lemon"], "melon" ], [ "orange", "grape" ], "pineapple" ];
+
+$iteriter = new RecursiveIteratorIterator(new RecursiveArrayIterator($tree));
+
+foreach ($iteriter as $key => $value) {
+  $d = $iteriter->getDepth();
+  echo "depth=$d k=$key v=$value\n";
+}
+
+echo "Done.";
