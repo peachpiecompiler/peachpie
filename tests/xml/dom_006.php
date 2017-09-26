58a56a4805dfd770ad06aa684db4c16aea3e6dc3
Robert Husák <robert@husak.cloud>
2017-09-26 16:32:51 +0200

    Enable read only DOMNodeList array access
    Although in PHP it doesn't really implement ArrayAccess, it can be
    accessed this way.

---
+++ tests/xml/dom_006.php
@@ -0,0 +1,27 @@
+<?php
+
+function test() {
+  $html = "<div>data</div>";
+  $doc = new DOMDocument;
+  $doc->loadHTML($html);
+  
+  $list = $doc->getElementsByTagName('div');
+  
+  echo isset($list[0]) ? "TRUE" : "FALSE";
+  echo $list[0]->textContent;
+  
+  echo isset($list["0"]) ? "TRUE" : "FALSE";
+  echo $list["0"]->textContent;
+  
+  echo isset($list["0.1234"]) ? "TRUE" : "FALSE";
+  echo $list["0.1234"]->textContent;
+  
+  echo isset($list["some string"]) ? "TRUE" : "FALSE";
+  echo $list["some string"]->textContent;
+  
+  echo isset($list[1]) ? "TRUE" : "FALSE";
+  
+  echo isset($list["1.5"]) ? "TRUE" : "FALSE";
+}
+
+test();
