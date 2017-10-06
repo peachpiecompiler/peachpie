fcc030720e8871087f5ad50f217575b9cafcf3aa
Robert Husák <robert@husak.cloud>
2017-10-06 15:45:20 +0200

    Add XSLT test on __autoload

---
+++ tests/xml/bug_33853.php
@@ -0,0 +1,28 @@
+<?php
+
+function __autoload($className) {
+  echo $className;
+  exit();
+}
+
+function test() {
+  $xsl = new DomDocument();
+  $xsl->loadXML('<?xml version="1.0" encoding="iso-8859-1" ?>
+  <xsl:stylesheet version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:php="http://php.net/xsl">
+  <xsl:template match="/">
+  <xsl:value-of select="php:function(\'TeSt::dateLang\')" />
+  </xsl:template>
+  </xsl:stylesheet>');
+  $inputdom = new DomDocument();
+  $inputdom->loadXML('<?xml version="1.0" encoding="iso-8859-1" ?>
+  <today></today>');
+
+  $proc = new XsltProcessor();
+  $proc->registerPhpFunctions();
+  $xsl = $proc->importStylesheet($xsl);
+  $newdom = $proc->transformToDoc($inputdom);
+}
+
+test();
