3ce943668b9a7daad4b42b530ede51c779960aa9
Robert Husák <robert@husak.cloud>
2017-11-19 19:28:49 +0100

    Implement Exception::getTraceAsString()

--- src/Peachpie.Library/Exceptions/Exception.cs
+++ src/Peachpie.Library/Exceptions/Exception.cs
@@ -51,10 +51,7 @@ namespace Pchp.Library.Spl
             throw new NotImplementedException();
         }
 
-        public virtual string getTraceAsString()
-        {
-            throw new NotImplementedException();
-        }
+        public virtual string getTraceAsString() => this.StackTrace;
 
         public virtual string __toString()
         {
