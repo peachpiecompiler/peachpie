a11f458595aacc35967a16920c203dc9f334c6e2
Jakub Míšek <jmisek@gmail.com>
2017-11-24 16:56:55 +0100

    test - instance call

---
+++ tests/functions/instance_call_002.php
@@ -0,0 +1,19 @@
+<?php
+
+interface IA {
+  public function foo();
+}
+
+class A implements IA {
+  public function foo() {
+    print_r( func_get_args() );
+  }
+}
+
+function test(IA $ia) {
+  $ia->foo("lorem", "ipsum");	// be careful, do not call IA::foo() directly
+}
+
+test(new A);
+
+echo "Done.";
