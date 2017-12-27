b42b6822bbe14ef8bfc3c71b8db48f715ab5e900
Jakub Míšek <jmisek@gmail.com>
2017-12-27 16:41:17 +0100

    generator tests
    - auto-incremented keys
    - yield from keys

---
+++ tests/generators/generators_015.php
@@ -0,0 +1,29 @@
+<?php
+function count_to_ten() {
+    yield 1;
+    yield 2;
+    yield from [3, 4];
+    yield from new ArrayIterator([5, 6]);
+    yield from seven_eight();
+    return yield from nine_ten();
+}
+
+function seven_eight() {
+    yield 7;
+    yield from eight();
+}
+
+function eight() {
+    yield 8;
+}
+
+function nine_ten() {
+    yield 9;
+    return 10;
+}
+
+$gen = count_to_ten();
+foreach ($gen as $num) {
+    echo "$num ";
+}
+echo $gen->getReturn();
