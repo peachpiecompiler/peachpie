3f6e269c09a98d1b69c9dae387f21746f90a7b38
Jakub Míšek <jmisek@gmail.com>
2018-02-01 16:03:50 +0100

    `__invoke` test

--- tests/classes/__invoke_002.php
+++ tests/classes/__invoke_002.php
@@ -1,5 +1,9 @@
 <?php
 
+interface I {
+	function __invoke();
+}
+
 class A {
     public function __invoke(...$args) {
         print_r( $args );
@@ -24,7 +28,15 @@ class D {
     }
 }
 
+
+class E implements I {
+    public function __invoke() {
+        print_r( func_get_args() );
+    }
+}
+
 (new A)(1, 2, 3);
 (new B)(1, 2, 3);
 (new C)(1, 2, 3);
 (new D)(1, 2, 3);
+(new E)(1, 2, 3);
