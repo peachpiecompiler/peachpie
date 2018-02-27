f2d1c83151898a5c92579e1a82b198ad4d8fbd39
Jakub Míšek <jmisek@gmail.com>
2018-02-27 18:55:39 +0100

    network library (empty)
    - placeholder for curl;ldap;ftp;smtp;sockets extensions

---
+++ src/Peachpie.Library.Network/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("Peachpie.Library.Network")]
+[assembly: AssemblyTrademark("")]
+
+// annotates this library as a php extension,
+// all its public static methods with compatible signatures will be seen as global functions to php scope
+[assembly: Pchp.Core.PhpExtension()]
