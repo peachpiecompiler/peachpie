b1d769b4ac27c65ec36e012f742db3da75097d6b
Jakub Míšek <jmisek@gmail.com>
2018-02-27 19:20:23 +0100

    CURLFile, curl_file_create

---
+++ src/Peachpie.Library.Network/CURLFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pchp.Core;
+
+namespace Peachpie.Library.Network
+{
+    /// <summary>
+    /// Object that should be used to upload a file with <c>curl</c> <c>CURLOPT_POSTFIELDS</c> option.
+    /// </summary>
+    [PhpType(PhpTypeAttribute.InheritName)]
+    public sealed class CURLFile
+    {
+        public string name { get; private set; }
+
+        public string mime
+        {
+            get => _mime;   // default: "application/octet-stream";
+            set => _mime = value ?? string.Empty;
+        }
+        string _mime;
+
+        public string postname
+        {
+            get => _postname;   // default: name;
+            set => _postname = value ?? string.Empty;
+        }
+        string _postname;
+
+        //[PhpFieldsOnlyCtor]
+        //protected CURLFile() { }
+
+        public CURLFile(string filename, string mimetype = null, string postname = null) => __construct(filename, mimetype, postname);
+
+        public void __construct(string filename, string mimetype = null, string postname = null)
+        {
+            this.name = filename;
+            this.mime = mimetype;
+            this.postname = postname;
+        }
+
+        public string getFilename() => this.name;
+
+        public string getMimeType() => this.mime;
+        public void setMimeType(string mime) => this.mime = mime;
+
+        public string getPostFilename() => this.postname;
+        public void setPostFilename(string postname) => this.postname = postname;
+    }
+}
---
+++ src/Peachpie.Library.Network/CURLFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pchp.Core;
+
+namespace Peachpie.Library.Network
+{
+    //[PhpExtension("curl")]
+    public static class CURLFunctions
+    {
+        /// <summary>
+        /// Create a CURLFile object.
+        /// </summary>
+        [return: NotNull]
+        public static CURLFile/*!*/curl_file_create(string filename, string mimetype = null, string postname = null) => new CURLFile(filename, mimetype, postname);
+    }
+}
