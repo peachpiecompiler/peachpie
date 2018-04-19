f9844d7852486a04cc2041e6b6c87d69ca4e15d7
Jakub Míšek <jmisek@gmail.com>
2018-04-19 10:47:45 +0200

    test for isset() on fields through callsites

---
+++ tests/operators/isset_fields.php
@@ -0,0 +1,29 @@
+<?php
+
+class X {
+    function __get($key) { return ''; }
+    var $defined = '';
+    var $d2 = '';
+	var $dnull = '';
+}
+
+function o( $b )
+{
+	echo $b ? "true" : "false";
+	echo "\n";
+}
+
+function test($x) {
+    $x->dynamic = '';
+
+    $a = $x->something;
+
+    o( isset($a) );
+    o( isset($x->something) ); // in case of __get, the value is converted to boolean instead
+    o( isset($x->defined) );
+    o( isset($x->dynamic) );
+    o( isset($x->d2) );
+	o( isset($x->dnull) );
+}
+
+test(new X);
