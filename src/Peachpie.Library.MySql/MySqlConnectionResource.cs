999a83e9c8ebd006a51a3b665d82e860621b4729
Jason Nelson <jason@carbonmade.com>
2018-05-23 14:00:17 -0500

    Update Close to Dispose

--- src/Peachpie.Library.MySql/MySqlConnectionResource.cs
+++ src/Peachpie.Library.MySql/MySqlConnectionResource.cs
@@ -48,7 +48,7 @@ namespace Peachpie.Library.MySql
             var myreader = (MySqlDataReader)_pendingReader;
             if (myreader != null)
             {
-                myreader.Close();   // we have to call Close() on MySqlDataReader, it is declared as non-virtual!
+                myreader.Dispose();
                 _pendingReader = myreader = null;
             }
         }
