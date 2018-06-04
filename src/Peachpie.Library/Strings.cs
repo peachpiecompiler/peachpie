9b2bb92bc398695d0580cd053e0633231d0f03a0
Jakub Míšek <jmisek@gmail.com>
2018-06-04 14:14:49 +0200

    htmlspecialchars
    - `&nbsp;` as known entity

--- src/Peachpie.Library/Strings.cs
+++ src/Peachpie.Library/Strings.cs
@@ -2219,7 +2219,7 @@ namespace Pchp.Library
         /// <summary>
         /// List of known HTML entities without leading <c>&amp;</c> character when checking double encoded entities.
         /// </summary>
-        static readonly string[] known_entities = { "amp;", "lt;", "gt;", "quot;", "apos;", "hellip;" };
+        static readonly string[] known_entities = { "amp;", "lt;", "gt;", "quot;", "apos;", "hellip;", "nbsp;" };
 
         /// <summary>
         /// Converts special characters of substring to HTML entities.
