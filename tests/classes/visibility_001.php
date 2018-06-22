608f36febf048cd87a68e879c3f67617d0725711
Jakub Míšek <jmisek@gmail.com>
2018-06-22 11:40:57 +0200

    protected property accessibility
    fixes https://github.com/peachpiecompiler/peachpie/issues/225
    - protected properties are accessible by parent classes as well
    - test case added

---
+++ tests/classes/visibility_001.php
@@ -0,0 +1,16 @@
+<?php
+
+// https://github.com/peachpiecompiler/peachpie/issues/225
+
+class A  {
+    public function getSomeText() {
+        return $this->_privateMember.'-DEBUG';
+    }
+}
+class B extends A {
+    protected $_privateMember = "Hi there!";
+}
+
+echo (new B())->getSomeText();
+
+echo "Done.";
