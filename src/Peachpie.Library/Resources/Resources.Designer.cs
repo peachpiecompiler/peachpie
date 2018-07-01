608fd4993ccf8f35061ae37db605d8badd7c4b87
Jakub Míšek <jmisek@gmail.com>
2018-07-01 15:59:52 +0200

    resx update

--- src/Peachpie.Library/Resources/Resources.Designer.cs
+++ src/Peachpie.Library/Resources/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Pchp.Library.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Peachpie.Library.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Pchp.Library.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
