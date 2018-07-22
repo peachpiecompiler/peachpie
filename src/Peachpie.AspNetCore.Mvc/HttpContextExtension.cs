b959591dae0508d01ea0aa834cdadd9d2f591f50
Jakub Míšek <jmisek@gmail.com>
2018-07-22 16:00:42 +0200

    Partial helper
    - to be used in PHP code to render Razor Partial View

--- src/Peachpie.AspNetCore.Mvc/HttpContextExtension.cs
+++ src/Peachpie.AspNetCore.Mvc/HttpContextExtension.cs
@@ -64,6 +64,26 @@ namespace Peachpie.AspNetCore.Mvc
         }
 
         /// <summary>
+        /// Gets Razor Partial View output.
+        /// </summary>
+        /// <param name="phpContext">Current context. If used within PHP code, this is passed implicitly by compiler.</param>
+        /// <param name="viewName">Name of the razor view.</param>
+        /// <param name="model">Model object instance.</param>
+        /// <remarks>
+        /// Can be used within a PHP code.
+        /// Note current <see cref="HttpContext.RequestServices"/> must provide <see cref="ICompositeViewEngine"/>
+        /// and <see cref="ITempDataDictionaryFactory"/>. These services are susually added by <c>Mvc</c> services.
+        /// </remarks>
+        public static string Partial(this Context phpContext, string viewName, object model = null)
+        {
+            var httpcontext = phpContext.GetHttpContext() ?? throw new ArgumentException(nameof(phpContext));
+
+            var output = new StringWriter();
+            RenderPartialAsync(httpcontext, output, viewName, model).Wait();
+            return output.GetStringBuilder().ToString();
+        }
+
+        /// <summary>
         /// Renders Razor View or Partial View to the output.
         /// </summary>
         public static Task RenderPartialAsync(this HttpContext context, TextWriter output,
