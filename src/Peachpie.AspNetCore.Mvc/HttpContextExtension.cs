1a95623d08112da54b9264268025b15362b5183d
Jakub Míšek <jmisek@gmail.com>
2018-07-22 15:43:08 +0200

    RenderPartial helper
    - rendering Razor Partial View within HttpContext
    - to be used within PHP code to render a Razor View

--- src/Peachpie.AspNetCore.Mvc/HttpContextExtension.cs
+++ src/Peachpie.AspNetCore.Mvc/HttpContextExtension.cs
@@ -11,6 +11,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Pchp.Core;
+using Peachpie.AspNetCore.Web;
 
 namespace Peachpie.AspNetCore.Mvc
 {
@@ -19,39 +22,85 @@ namespace Peachpie.AspNetCore.Mvc
     /// </summary>
     public static class HttpContextExtension
     {
-        /// <summary>
-        /// Renders Razor View or Partial View to the output.
-        /// </summary>
-        public static Task RenderViewAsync<TModel>(this HttpContext context, TextWriter output, IViewEngine viewEngine, string viewName, TModel model)
+        #region nested class: PhpScriptRouter
+
+        sealed class PhpScriptRouter : IRouter
         {
-            var appctx = new ActionContext(context, new RouteData(), new ActionDescriptor());
+            public VirtualPathData GetVirtualPath(VirtualPathContext context) => null;
 
-            var view = viewEngine.FindView(appctx, viewName, isMainPage: false);
+            public Task RouteAsync(RouteContext context) => throw new NotSupportedException();
+        }
 
-            if (view.Success)
+        #endregion
+
+        #region nested class: CustomViewDataDictionary
+
+        sealed class CustomViewDataDictionary : ViewDataDictionary
+        {
+            public CustomViewDataDictionary(ViewDataDictionary source, object model, Type modelType)
+                : base(source, model, modelType)
             {
-                return RenderViewAsync(context, output, view.View, model);
             }
-            else
-            {
-                throw new ArgumentException(nameof(viewName));
-            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Renders Razor Partial View to the output.
+        /// </summary>
+        /// <param name="phpContext">Current context. If used within PHP code, this is passed implicitly by compiler.</param>
+        /// <param name="viewName">Name of the razor view.</param>
+        /// <param name="model">Model object instance.</param>
+        /// <remarks>
+        /// Can be used within a PHP code.
+        /// Note current <see cref="HttpContext.RequestServices"/> must provide <see cref="ICompositeViewEngine"/>
+        /// and <see cref="ITempDataDictionaryFactory"/>. These services are susually added by <c>Mvc</c> services.
+        /// </remarks>
+        public static void RenderPartial(this Context phpContext, string viewName, object model = null)
+        {
+            var httpcontext = phpContext.GetHttpContext() ?? throw new ArgumentException(nameof(phpContext));
+
+            RenderPartialAsync(httpcontext, phpContext.Output, viewName, model).Wait();
         }
 
         /// <summary>
         /// Renders Razor View or Partial View to the output.
         /// </summary>
-        public static Task RenderViewAsync<TModel>(this HttpContext context, TextWriter output, IView view, TModel model)
+        public static Task RenderPartialAsync(this HttpContext context, TextWriter output,
+            string viewName, object model = null,
+            ViewDataDictionary viewdata = null,
+            IViewEngine viewEngine = null,
+            ITempDataDictionaryFactory tmpdataFactory = null,
+            HtmlHelperOptions htmlOptions = null)
         {
-            var viewdata = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()); // TODO: as parameter
+            // dummy router:
+            var routedata = new RouteData();
+            routedata.Routers.Add(new PhpScriptRouter());
 
-            return view.RenderAsync(new ViewContext()
+            // find the view:
+            var action = new ActionContext(context, routedata, new ActionDescriptor());
+            viewEngine = viewEngine ?? context.RequestServices.GetService<IViewEngine>();
+            var view = viewEngine.FindView(action, viewName, isMainPage: false);
+
+            if (view.Success)
             {
-                HttpContext = context,
-                View = view,
-                Writer = output,
-                ViewData = new ViewDataDictionary<TModel>(viewdata, model)
-            });
+                // render the view:
+                tmpdataFactory = tmpdataFactory ?? context.RequestServices.GetService<ITempDataDictionaryFactory>();
+                viewdata = viewdata ?? new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary());
+
+                var viewctx = new ViewContext(action, view.View,
+                    new CustomViewDataDictionary(viewdata, model, model != null ? model.GetType() : typeof(object)),
+                    tmpdataFactory.GetTempData(context),
+                    output,
+                    htmlOptions ?? new HtmlHelperOptions { });
+
+                return view.View.RenderAsync(viewctx);
+            }
+            else
+            {
+                throw new ArgumentException(nameof(viewName));
+            }
+
         }
     }
 }
