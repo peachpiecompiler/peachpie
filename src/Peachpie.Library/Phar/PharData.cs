66b460ca02e22d40a04ece49c03cdc7c9bc03461
Jakub Míšek <jmisek@gmail.com>
2018-09-27 00:52:41 +0200

    dummy phar classes
    related to https://github.com/peachpiecompiler/peachpie/issues/83

---
+++ src/Peachpie.Library/Phar/PharData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Pchp.Core;
+using Pchp.Library.Spl;
+
+namespace Pchp.Library.Phar
+{
+    /// <summary>
+    /// The <see cref="PharData"/> class provides a high-level interface to accessing and creating non-executable tar and zip archives.
+    /// </summary>
+    [PhpType(PhpTypeAttribute.InheritName)]
+    [PhpExtension(PharExtension.ExtensionName)]
+    public sealed class PharData /* : RecursiveDirectoryIterator*/
+    {
+        #region .ctor
+
+        public PharData(string fname, int flags = 0, string alias = default, int format = 0)
+        {
+            __construct(fname, flags, alias, format);
+        }
+
+        public void __construct(string fname, int flags = 0, string alias = default, int format = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Peachpie.Library/Phar/PharFileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Pchp.Core;
+using Pchp.Library.Spl;
+
+namespace Pchp.Library.Phar
+{
+    /// <summary>
+    /// The <see cref="PharFileInfo"/> class provides a high-level interface to the contents and attributes of a single file within a phar archive.
+    /// </summary>
+    [PhpType(PhpTypeAttribute.InheritName)]
+    [PhpExtension(PharExtension.ExtensionName)]
+    public sealed class PharFileInfo : SplFileInfo
+    {
+        public PharFileInfo(Context ctx, string entry) : base(ctx, entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
