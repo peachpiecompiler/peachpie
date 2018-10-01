6f4b4244d504feedadd85f64416c651952abefa5
Jakub Míšek <jmisek@gmail.com>
2018-10-01 16:11:42 +0200

    token_name

---
+++ src/Peachpie.Library.Scripting/Tokenizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pchp.Core;
+
+namespace Peachpie.Library.Scripting
+{
+    [PhpExtension("tokenizer")]
+    public static class Tokenizer
+    {
+        // TODO: T_*** constants
+
+        /// <summary>
+        /// Get the symbolic name of a given PHP token.
+        /// </summary>
+        public static string token_name(int token)
+        {
+            return ((Devsense.PHP.Syntax.Tokens)token).ToString();
+        }
+    }
+}
