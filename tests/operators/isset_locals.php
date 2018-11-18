747605821c05d9ce90ad19b0e0e65a3fcf1dbd26
Jakub Míšek <jmisek@gmail.com>
2018-11-18 16:16:40 +0100

    test: extract & isset

---
+++ tests/operators/isset_locals.php
@@ -0,0 +1,19 @@
+<?php
+
+function foo( $arr ) {
+	// "extract" introduces locals dynamically,
+	// make sure the analysis won't break
+	extract( $arr );
+	
+	if (isset($x)) {
+		echo "isset\n";
+	}
+	else {
+		echo "notset\n";
+	}		
+}
+
+foo( ["x" => 1] );
+foo( ["y" => 1] );
+
+echo "Done.";
