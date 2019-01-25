0430ab5a27fad7e0d3c99d6f993facf98614dafb
Jakub Míšek <jmisek@gmail.com>
2019-01-25 18:10:51 +0100

    Create instance_call_004.php

---
+++ tests/functions/instance_call_004.php
@@ -0,0 +1,24 @@
+<?php
+
+class X {
+    private function handleDependencies() : bool {
+        echo "1.";
+        return false;
+    }
+
+    function test() {
+        if (!$this instanceof Y)
+        {
+            // lets confuse type analysis with $this
+        }
+
+        $this->handleDependencies(); // call to a private method so $this variable must be resolved to be able to call it in ct
+    }
+}
+
+class Y extends X {
+}
+
+(new X)->test();
+
+echo "Done.";
