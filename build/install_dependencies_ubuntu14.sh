aa06e42eba5ff7d06bed0503bd4b9180288ceb2d
Jakub Míšek <jmisek@gmail.com>
2019-04-19 09:23:30 +0200

    Update install_dependencies_ubuntu14.sh

--- build/install_dependencies_ubuntu14.sh
+++ build/install_dependencies_ubuntu14.sh
@@ -1,13 +1,13 @@
 # Install .NET Core # https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-current
 
 # Register Microsoft key and feed
-wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
 # Install .NET Core SDK
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1.600 -y
+sudo apt-get install dotnet-sdk-2.1 -y
 
 # Install Powershell (https://www.rootusers.com/how-to-install-powershell-on-linux)
 
