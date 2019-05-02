8e14bfed757bfd80ef5b9b42c0bfc5aa7e85faa9
Robert Husák <robert@husak.cloud>
2019-05-02 14:51:42 +0200

    Fix RecursiveDirectoryIterator_001.php test

--- tests/spl/RecursiveDirectoryIterator_001.php
+++ tests/spl/RecursiveDirectoryIterator_001.php
@@ -6,10 +6,15 @@ function test() {
   $it = new \RecursiveIteratorIterator($it);
 
   foreach ($it as $file) {
+    $name = $file->getFilename();
+    if ($name == "." || $name == "..") {
+      continue;
+    }
+
     for ($i = 0; $i < $it->getDepth(); $i++) {
       echo "  ";
     }
-  	echo $file->getFilename() ."\n";
+  	echo $name ."\n";
   }
 }
 
