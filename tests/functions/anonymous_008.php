3133bef88d65fb2f94e16862e06f4be8e4349861
Robert Husák <robert@husak.cloud>
2019-06-19 23:45:04 +0200

    Add test of $this in Generator in anonymous function
    - test case of #449

---
+++ tests/functions/anonymous_008.php
@@ -0,0 +1,22 @@
+<?php
+
+class A
+{
+  private $foo;
+
+  private function bar()
+  {
+    return function() {
+      $this->foo = 42;
+      yield 42;
+    };
+  }
+
+  public function test() {
+    foreach ($this->bar()() as $v) {
+      echo $v;
+    }
+  }
+}
+
+(new A)->test();
