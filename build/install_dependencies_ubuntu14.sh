d16391310cffb10bd82d73390958e41d2f5a16c7
Jakub Míšek <jmisek@gmail.com>
2019-10-15 13:04:40 +0200

    asp.net core support to [2.1..), tests on core 3.0

--- build/install_dependencies_ubuntu14.sh
+++ build/install_dependencies_ubuntu14.sh
@@ -5,9 +5,10 @@ wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu
 sudo dpkg -i packages-microsoft-prod.deb
 
 # Install .NET Core SDK
+sudo apt-get update
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1 -y
+sudo apt-get install dotnet-sdk-3.0 -y
 
 # Install Powershell (https://www.rootusers.com/how-to-install-powershell-on-linux)
 
