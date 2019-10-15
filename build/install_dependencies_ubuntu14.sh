8e9b8d15eeb40b1c81cead043a1386b7469b8add
Jakub Míšek <jmisek@gmail.com>
2019-10-15 16:22:50 +0200

    travis update #2

--- build/install_dependencies_ubuntu14.sh
+++ build/install_dependencies_ubuntu14.sh
@@ -6,20 +6,15 @@ wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 
 # Install .NET Core SDK
-sudo apt-get update
 sudo apt-get install apt-transport-https
-sudo apt-get update
 sudo apt-get install dotnet-sdk-3.0 -y
 
 # Install Powershell (https://www.rootusers.com/how-to-install-powershell-on-linux)
 
-# Install Powershell dependencies
-sudo apt-get install libunwind8 libicu52 liblttng-ust0 -y
-
 # Download and install Powershell
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.1.6/powershell_6.1.6-1.ubuntu.14.04_amd64.deb
-sudo dpkg -i powershell_6.1.6-1.ubuntu.14.04_amd64.deb
-rm -f powershell_6.1.6-1.ubuntu.14.04_amd64.deb
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.3/powershell_6.2.3-1.ubuntu.16.04_amd64.deb
+sudo dpkg -i powershell_6.2.3-1.ubuntu.16.04_amd64.deb
+rm -f powershell_6.2.3-1.ubuntu.16.04_amd64.deb
 
 # Install Python Pip and icdiff (http://www.jefftk.com/icdiff)
 
