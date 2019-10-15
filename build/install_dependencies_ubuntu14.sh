16cbdb9ac730b8e91444d59b71263d66d447a8a5
Jakub Míšek <jmisek@gmail.com>
2019-10-15 15:59:35 +0200

    travis fix #2

--- build/install_dependencies_ubuntu14.sh
+++ build/install_dependencies_ubuntu14.sh
@@ -1,9 +1,16 @@
 # Install .NET Core # https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-current
 
 # Register Microsoft key and feed
-wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+#wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
 #sudo dpkg -i packages-microsoft-prod.deb
-sudo dpkg --purge packages-microsoft-prod && sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get install -y gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 
 # Install .NET Core SDK
 sudo apt-get update
