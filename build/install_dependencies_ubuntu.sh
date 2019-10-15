c66c82967482e8a5b7df373455355aadb295759c
Jakub Míšek <jmisek@gmail.com>
2019-10-15 16:34:04 +0200

    travis fixes #6

--- build/install_dependencies_ubuntu.sh
+++ build/install_dependencies_ubuntu.sh
@@ -6,10 +6,14 @@ wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 
 # Install .NET Core SDK
+sudo apt-get update
 sudo apt-get install apt-transport-https
+sudo apt-get update
 sudo apt-get install dotnet-sdk-3.0 -y
 
-# Install Powershell (https://www.rootusers.com/how-to-install-powershell-on-linux)
+# Install Powershell
+# https://www.rootusers.com/how-to-install-powershell-on-linux
+sudo apt-get install libunwind8 libicu55 -y
 
 # Download and install Powershell
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.3/powershell_6.2.3-1.ubuntu.16.04_amd64.deb
