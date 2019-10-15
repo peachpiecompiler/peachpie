4717057ce27c3b9bf33cf6e85f3f49b15e310ce8
Jakub Míšek <jmisek@gmail.com>
2019-10-15 12:29:04 +0200

    build on 3.0 fixes

--- build/devps.cmd
+++ build/devps.cmd
@@ -1,1 +1,1 @@
-cmd.exe /k ""C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" & powershell"
+cmd.exe /k ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" & powershell"
