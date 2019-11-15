8527198cf4070aa7cf595c5e281c88ab5605dc6d
Jakub Míšek <jmisek@gmail.com>
2019-11-15 13:01:16 +0100

    less overloads generated from traits
    overloads with optional argument not needed anymore
    
    Signed-off-by: Jakub Míšek <jmisek@gmail.com>

--- src/Peachpie.CodeAnalysis/CodeGen/Symbols/SourceTypeSymbol.cs
+++ src/Peachpie.CodeAnalysis/CodeGen/Symbols/SourceTypeSymbol.cs
@@ -531,16 +531,17 @@ namespace Pchp.CodeAnalysis.Symbols
                     }, null, DiagnosticBag.GetInstance(), false));
                     module.SynthesizedManager.AddMethod(this, m);
 
-                    // ghost stubs: // TODO: resolve this already in SourceTypeSymbol.GetMembers(), now it does not get overloaded properly
-                    var ps = m.Parameters;
-                    for (int i = 0; i < ps.Length; i++)
-                    {
-                        if (ps[i].HasUnmappedDefaultValue())  // => ConstantValue couldn't be resolved for optional parameter
-                        {
-                            // create ghost stub foo(p0, .. pi-1) => foo(p0, .. , pN)
-                            GhostMethodBuilder.CreateGhostOverload(m, module, DiagnosticBag.GetInstance(), i);
-                        }
-                    }
+                    // NOTE: following is not needed anymore:
+                    //// ghost stubs: // ... resolve this already in SourceTypeSymbol.GetMembers(), now it does not get overloaded properly
+                    //var ps = m.Parameters;
+                    //for (int i = 0; i < ps.Length; i++)
+                    //{
+                    //    if (ps[i].HasUnmappedDefaultValue())  // => ConstantValue couldn't be resolved for optional parameter
+                    //    {
+                    //        // create ghost stub foo(p0, .. pi-1) => foo(p0, .. , pN)
+                    //        GhostMethodBuilder.CreateGhostOverload(m, module, DiagnosticBag.GetInstance(), i);
+                    //    }
+                    //}
                 }
             }
         }
