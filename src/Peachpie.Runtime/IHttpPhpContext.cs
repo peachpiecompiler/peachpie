fc9ad30fb8349449acba749608c2e986f98f71c5
Jakub Míšek <jmisek@gmail.com>
2020-01-16 18:30:02 +0100

    nullable annotations

--- src/Peachpie.Runtime/IHttpPhpContext.cs
+++ src/Peachpie.Runtime/IHttpPhpContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -57,7 +59,7 @@ namespace Pchp.Core
         /// <summary>
         /// Adds a cookie into the response.
         /// </summary>
-        void AddCookie(string name, string value, DateTimeOffset? expires, string path = "/", string domain = null, bool secure = false, bool httpOnly = false);
+        void AddCookie(string name, string value, DateTimeOffset? expires, string path = "/", string? domain = null, bool secure = false, bool httpOnly = false);
 
         /// <summary>
         /// Flushes the response stream of the HTTP server.
