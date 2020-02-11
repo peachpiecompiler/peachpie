a4f6b90a661379929010c51936563a55a95738ab
Robert Husák <robert@husak.cloud>
2020-02-11 16:57:56 +0100

    Make preg_replace_callback test more thorough
    Check that the callback isn't called more than once, see #666

--- tests/pcre/preg_replace_callback_001.php
+++ tests/pcre/preg_replace_callback_001.php
@@ -3,6 +3,7 @@ namespace pcre\preg_replace_callback_001;
 
 function f() {
 	echo preg_replace_callback('~-([a-z])~', function ($match) {
+    echo "callback\n";
 		return strtoupper($match[1]);
 	}, 'hello-world');
 }
