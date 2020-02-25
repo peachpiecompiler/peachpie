c2e15649a8bbb199876ff00b5e0dec8553009447
Jakub Míšek <jmisek@gmail.com>
2020-02-25 18:57:17 +0100

    test for https://github.com/peachpiecompiler/peachpie/issues/684

--- tests/variables/isset_005.php
+++ tests/variables/isset_005.php
@@ -3,7 +3,10 @@ namespace variables\isset_005;
 
 function test($name)
 {
-    $obj = (object)[$name => null];
+    $obj = (object)[$name => null, ];
+    echo "isset: ", isset( $obj->$name ) ? "1" : "0", PHP_EOL;
+    
+    $obj = (object)[$name => true, ];
     echo "isset: ", isset( $obj->$name ) ? "1" : "0", PHP_EOL;
 }
 
