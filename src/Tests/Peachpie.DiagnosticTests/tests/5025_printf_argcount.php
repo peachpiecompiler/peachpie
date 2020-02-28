a755c11428784209bc345604c34888daab952a10
Jakub Míšek <jmisek@gmail.com>
2020-02-29 00:40:10 +0100

    5025 tests updated
    warning reported at the argument span

--- src/Tests/Peachpie.DiagnosticTests/tests/5025_printf_argcount.php
+++ src/Tests/Peachpie.DiagnosticTests/tests/5025_printf_argcount.php
@@ -2,13 +2,13 @@
 
 function foo($a, $b, $c, $d) {
   printf("%d %f %'.9d", $a, $b, $c);
-  printf("%d %f %'.9d", $a, $b)/*!PHP5025!*/;
-  printf("%d %f %'.9d", $a, $b, $c, $d)/*!PHP5025!*/;
-  printf('%1$d %d %2$f', $a, $b, $c)/*!PHP5025!*/;
-  printf('%1$d %d %1$f', $a, $b)/*!PHP5025!*/;
+  printf("%d %f %'.9d"/*!PHP5025!*/, $a, $b);
+  printf("%d %f %'.9d"/*!PHP5025!*/, $a, $b, $c, $d);
+  printf('%1$d %d %2$f'/*!PHP5025!*/, $a, $b, $c);
+  printf('%1$d %d %1$f'/*!PHP5025!*/, $a, $b);
   printf('%1$d %1$d', $a);
 
   echo sprintf("%%%d%%%%%f", $a, $b);
-  echo sprintf("%%%d%%%%%f", $a)/*!PHP5025!*/;
-  echo sprintf("%%%d%%%%%f", $a, $b, $c)/*!PHP5025!*/;
+  echo sprintf("%%%d%%%%%f"/*!PHP5025!*/, $a);
+  echo sprintf("%%%d%%%%%f"/*!PHP5025!*/, $a, $b, $c);
 }
