8a9c2bce14c625a8f0d6b71f6653cda93ad2e1d3
Jakub Míšek <jmisek@gmail.com>
2020-03-18 23:36:47 +0100

    test fixed

--- tests/reflection/class_methods_001.php
+++ tests/reflection/class_methods_001.php
@@ -18,12 +18,12 @@ class C
     public function publicMethod(){}
     public function __construct()
     {
-        print_r(in_array('privateMethod', get_class_methods('C')));
+        print_r(in_array('privateMethod', get_class_methods(__NAMESPACE__.'\C')));
 		print_r(method_exists($this, 'privateMethod'));
     }
 }
 
-print_r(in_array('__construct', get_class_methods('myclass')));
+print_r(in_array('__construct', get_class_methods(__NAMESPACE__.'\myclass')));
 print_r(in_array('myfunc2', get_class_methods(new myclass)));
 print_r(in_array('privateMethod', get_class_methods(new C)));
-print_r(method_exists('C', 'privateMethod'));
+print_r(method_exists(__NAMESPACE__.'\C', 'privateMethod'));
