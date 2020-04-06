3e02522916fbf88f612fda18e2532a07f22ef07c
Jakub Míšek <jmisek@gmail.com>
2020-04-06 16:12:52 +0200

    compacted SynthesizedFieldSymbol

--- src/Peachpie.CodeAnalysis/Symbols/Synthesized/SynthesizedFieldSymbol.cs
+++ src/Peachpie.CodeAnalysis/Symbols/Synthesized/SynthesizedFieldSymbol.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,26 @@ namespace Pchp.CodeAnalysis.Symbols
 {
     class SynthesizedFieldSymbol : FieldSymbol
     {
+        [Flags]
+        enum PackedFlags : byte
+        {
+            //
+            // 000|r|s|aaa
+            //
+
+            /// <summary>
+            /// See <see cref="Accessibility"/>.
+            /// </summary>
+            AccessibilityMask = 0x7, // aaa
+
+            IsStaticBit = 1 << 3,    // s
+            IsReadOnlyBit = 1 << 4,  // r
+        }
+
         readonly NamedTypeSymbol _containing;
         TypeSymbol _type;
         readonly string _name;
-        readonly Accessibility _accessibility;
-        readonly bool _isStatic, _isReadOnly;
+        readonly PackedFlags _flags;
         readonly ConstantValue _const;
 
         public SynthesizedFieldSymbol(
@@ -29,9 +45,18 @@ namespace Pchp.CodeAnalysis.Symbols
             _containing = containing;
             _name = name;
             _type = type;
-            _accessibility = accessibility;
-            _isStatic = isStatic;
-            _isReadOnly = isReadOnly;
+
+            //
+
+            _flags =
+                ((PackedFlags)accessibility) |
+                (isStatic ? PackedFlags.IsStaticBit : 0) |
+                (isReadOnly ? PackedFlags.IsReadOnlyBit : 0)
+                ;
+
+            Debug.Assert(DeclaredAccessibility == accessibility);
+            Debug.Assert(IsStatic == isStatic);
+            Debug.Assert(IsReadOnly == isReadOnly);
         }
 
         public SynthesizedFieldSymbol(
@@ -51,15 +76,15 @@ namespace Pchp.CodeAnalysis.Symbols
 
         public override ImmutableArray<CustomModifier> CustomModifiers => ImmutableArray<CustomModifier>.Empty;
 
-        public override Accessibility DeclaredAccessibility => _accessibility;
+        public override Accessibility DeclaredAccessibility => (Accessibility)(_flags & PackedFlags.AccessibilityMask);
 
         public override ImmutableArray<SyntaxReference> DeclaringSyntaxReferences => ImmutableArray<SyntaxReference>.Empty;
 
         public override bool IsConst => _const != null;
 
-        public override bool IsReadOnly => _isReadOnly;   // .initonly
+        public override bool IsReadOnly => (_flags & PackedFlags.IsReadOnlyBit) != 0;   // .initonly
 
-        public override bool IsStatic => _isStatic;
+        public override bool IsStatic => (_flags & PackedFlags.IsStaticBit) != 0;
 
         public override bool IsVolatile => false;
 
