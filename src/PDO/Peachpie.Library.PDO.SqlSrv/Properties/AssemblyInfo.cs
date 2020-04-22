8e67fd9bb94b9a1b3c08ca0096da999d22048dce
Jakub Míšek <jmisek@gmail.com>
2020-04-22 09:57:37 +0200

    pdo drivers annotated

--- src/PDO/Peachpie.Library.PDO.SqlSrv/Properties/AssemblyInfo.cs
+++ src/PDO/Peachpie.Library.PDO.SqlSrv/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Pchp.Core;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTrademark("")]
+[assembly: PhpExtension("pdo_sqlsrv")]
