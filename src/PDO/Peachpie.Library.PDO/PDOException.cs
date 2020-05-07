9780b324c3fe3dfe20a3441537ccce1e3bd49fc5
Jakub Míšek <jmisek@gmail.com>
2020-05-07 21:48:53 +0200

    PDOException - code, errorInfo
    fixes https://github.com/peachpiecompiler/peachpie/issues/746
    - $code = SQLSTATE
    - $errorInfo can be set
    - hides internal fields

--- src/PDO/Peachpie.Library.PDO/PDOException.cs
+++ src/PDO/Peachpie.Library.PDO/PDOException.cs
@@ -1,4 +1,6 @@
-﻿using Pchp.Library.Spl;
+﻿#nullable enable
+
+using Pchp.Library.Spl;
 using System;
 using Pchp.Core;
 
@@ -14,10 +16,24 @@ namespace Peachpie.Library.PDO
     {
         /// <summary>
         /// Corresponds to <see cref="PDO.errorInfo"/> or <see cref="PDOStatement.errorInfo"/>.
+        /// Can be changed.
         /// </summary>
-        public PhpArray errorInfo => _error.ToPhpErrorInfo();
+        public PhpArray errorInfo
+        {
+            get => _errorInfo ?? _error.ToPhpErrorInfo();
+            /*protected*/set => _errorInfo = value;
+        }
 
+        [PhpHidden]
+        private PhpArray? _errorInfo;
+
+        [PhpHidden]
         readonly PDO.ErrorInfo _error;
+
+        /// <summary>
+        /// SQLSTATE error code.
+        /// </summary>
+        public virtual new string getCode() => this.code.ToString(); // _error.SqlState
 
         /// <summary>
         /// Empty constructor used when overriding class in PHP.
@@ -25,10 +41,11 @@ namespace Peachpie.Library.PDO
         [PhpFieldsOnlyCtor]
         protected PDOException() { }
 
-        internal PDOException(PDO.ErrorInfo error, Throwable previous = null)
-            : base(error.Message, error.CodeOrZero(), previous)
+        internal PDOException(PDO.ErrorInfo error, Throwable? previous = null)
+            : base(error.Message, 0, previous)
         {
             _error = error;
+            this.code = error.SqlState;
         }
 
         /// <summary>
@@ -37,9 +54,10 @@ namespace Peachpie.Library.PDO
         /// <param name="message">The message.</param>
         /// <param name="code">The code.</param>
         /// <param name="previous">The previous.</param>
-        public PDOException(string message = "", long code = 0, Throwable previous = null)
-            : this(PDO.ErrorInfo.Create(string.Empty, code.ToString(), message), previous)
+        public PDOException(string message = "", long code = 0, Throwable? previous = null)
+            : this(PDO.ErrorInfo.Create(string.Empty, string.Empty, message), previous)
         {
+            this.code = code;
         }
     }
 }
