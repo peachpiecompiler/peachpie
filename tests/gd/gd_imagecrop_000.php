929c5363ddfc7cff61048eb3612f531edec03b4a
Tomas Husak <tomikhusak@seznam.com>
2020-05-09 18:00:23 +0200

    Tests - imagescale,imageflip,imagecrop

---
+++ tests/gd/gd_imagecrop_000.php
@@ -0,0 +1,73 @@
+<?php
+// Creates img
+$width = 9;
+$height = 9;
+$img = imagecreatetruecolor($width, $height);
+
+//functions
+function draw_init_symbol($img)
+{
+    $width  = imagesx($img);
+    $height = imagesy($img);
+    $color = imagecolorallocate($img, 255, 255, 255);
+
+    // Draw verticle axis
+    for ($j = 0; $j <  $height; $j++)
+        imagesetpixel($img, $width/2, $j, $color);
+
+    //imageline($img, $width/2, 0, $width/2, $height -1, $color);
+    
+    // Draw decreasing axis
+    for ($j = 0;$j <  $height; $j++)
+        imagesetpixel($img, $j, $j, $color);
+    
+    //imageline($img, 1, $height -2, $width -2, 1, $color);
+
+    return $img;
+}
+
+function print_img_pixels($img)
+{
+    $width  = imagesx($img);
+    $height = imagesy($img);
+    for ($i = 0;$i <  $width;$i++)
+    {
+        echo "Row "  . $i . ": "; 
+        for ($j = 0;$j <  $height;$j++)
+        {
+            echo imagecolorat($img, $i, $j) . " ";
+        }
+        echo "\n";
+    }
+}
+
+function transform_img($img)
+{
+    $size = min(imagesx($img), imagesy($img));
+
+    return imagecrop($img, ['x' => 1, 'y' => 1, 'width' => $size -2, 'height' => $size -2]);
+}
+// Init
+$init_img = draw_init_symbol($img);
+
+//Prints
+echo "Before transformation img(". imagesx($init_img) . "x". imagesy($init_img) . "):\n";
+print_img_pixels($init_img);
+
+// Transforms img
+$transformed = transform_img($init_img);
+
+// Image in browser
+//header('Content-type: image/png');
+//imagepng($transformed);
+
+//Prints
+if (!$transformed)
+    echo "Transformation failed";
+else
+{
+    echo "After transformation img(". imagesx($transformed) . "x". imagesy($transformed) . "):\n";
+    print_img_pixels($transformed);
+}
+print_r(imagecolorsforindex($img,imagecolorat($img, 0, 0)));
+?>
