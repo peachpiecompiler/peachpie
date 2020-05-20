813c97f6f4d8aa4485660f32d34057f19db294ac
Jakub Míšek <jmisek@gmail.com>
2020-05-20 14:03:04 +0200

    sdk: reports composer.json warnings with location and description

--- src/Peachpie.NET.Sdk/SpdxHelpers.cs
+++ src/Peachpie.NET.Sdk/SpdxHelpers.cs
@@ -48,11 +48,25 @@ namespace Peachpie.NET.Sdk
         /// Handles common typos and invalid license strings,
         /// translates it to valid SPDX recognized by NuGet task.
         /// </summary>
-        public static string SanitizeSpdx(string spdx)
+        public static string SanitizeSpdx(string spdx) => SanitizeSpdx(spdx, out _);
+
+        /// <summary>
+        /// Handles common typos and invalid license strings,
+        /// translates it to valid SPDX recognized by NuGet task.
+        /// </summary>
+        public static string SanitizeSpdx(string spdx, out string warning)
         {
+            warning = null;
+
             if (string.IsNullOrEmpty(spdx))
             {
+                warning = "Empty value.";
                 return string.Empty;
+            }
+
+            string LicenseWarning(string old, string newspdx)
+            {
+                return $"License '{old}' is invalid. Resolved as '{newspdx}'. ";
             }
 
             // there are commonly used deprecations and invalid expressions,
@@ -60,6 +74,7 @@ namespace Peachpie.NET.Sdk
 
             if (s_spdx_fixes.TryGetValue(spdx, out var newspdx))
             {
+                warning = LicenseWarning(spdx, newspdx);
                 spdx = newspdx;
             }
             else if (spdx.IndexOfAny(s_spdx_separators) != -1) // might be an expression
@@ -89,6 +104,7 @@ namespace Peachpie.NET.Sdk
                                 // replace matched pair.Key with pair.Value:
                                 spdx = spdx.Remove(index) + pair.Value + spdx.Substring(index + pair.Key.Length);
                                 sep = index + pair.Value.Length;
+                                warning += LicenseWarning(pair.Key, pair.Value);
                                 break;
                             }
                         }
