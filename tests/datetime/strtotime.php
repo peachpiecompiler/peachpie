51dda40458688df01ddc715ac47c82eb13321a16
Indigo744 <contact-github@gperrin.fr>
2020-09-01 21:49:00 +0200

    strtotime: add more failing tests

--- tests/datetime/strtotime.php
+++ tests/datetime/strtotime.php
@@ -9,6 +9,8 @@ function test() {
     echo date("Y-m-d H:i", strtotime("+1 week 2 days 4 hours 2 seconds")) . PHP_EOL;
     echo date("Y-m-d H:i", strtotime("next Thursday")) . PHP_EOL;
     echo date("Y-m-d H:i", strtotime("last Monday")) . PHP_EOL;
+    echo date("Y-m-d H:i", strtotime("2020-07-14")) . PHP_EOL;
+    echo date("Y-m-d H:i", strtotime("2020-01")) . PHP_EOL;
     echo date("Y-m-d H:i", strtotime("first monday of 2020-01")) . PHP_EOL;
 }
 
