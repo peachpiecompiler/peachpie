cca0646f8f76c9376f32859eda835ece6fde8d52
Jakub Míšek <jmisek@gmail.com>
2020-10-24 12:20:53 +0200

    wordwrap - a failing test case

--- tests/strings/wordwrap.php
+++ tests/strings/wordwrap.php
@@ -16,5 +16,13 @@ echo(wordwrap("123      123ab123", 3, "|", true)), PHP_EOL;
 // handling newline:
 echo wordwrap("ab a\nbbbb\nccccc\nddd\neeee", 5), PHP_EOL;
 
+// test from manual with newline:
+$text = "The quick brown fox
+jumped
+over the lazy dog.";
+$newtext = wordwrap($text, 20, "<br />\n");
+
+echo $newtext, PHP_EOL;
+
 //
 echo "Done.";
