2f020dc2a711a79c21126e200148a55d39f115dd
Jakub Míšek <jmisek@gmail.com>
2020-10-24 12:11:33 +0200

    wordwrap test case
    ref https://github.com/peachpiecompiler/peachpie/pull/875

--- tests/strings/wordwrap.php
+++ tests/strings/wordwrap.php
@@ -1,14 +1,20 @@
 <?php
 namespace strings\wordwrap;
 
-echo(wordwrap("123  123ab123", 3, "|")),"\n";
-echo(wordwrap("123   123ab123", 3, "|")),"\n";
-echo(wordwrap("123    123ab123", 3, "|")),"\n";
-echo(wordwrap("123     123ab123", 3, "|")),"\n";
-echo(wordwrap("123      123ab123", 3, "|")),"\n";
+echo(wordwrap("123  123ab123", 3, "|")), PHP_EOL;
+echo(wordwrap("123   123ab123", 3, "|")), PHP_EOL;
+echo(wordwrap("123    123ab123", 3, "|")), PHP_EOL;
+echo(wordwrap("123     123ab123", 3, "|")), PHP_EOL;
+echo(wordwrap("123      123ab123", 3, "|")), PHP_EOL;
 
-echo(wordwrap("123  123ab123", 3, "|", true)),"\n";
-echo(wordwrap("123   123ab123", 3, "|", true)),"\n";
-echo(wordwrap("123    123ab123", 3, "|", true)),"\n";
-echo(wordwrap("123     123ab123", 3, "|", true)),"\n";
-echo(wordwrap("123      123ab123", 3, "|", true)),"\n";
+echo(wordwrap("123  123ab123", 3, "|", true)), PHP_EOL;
+echo(wordwrap("123   123ab123", 3, "|", true)), PHP_EOL;
+echo(wordwrap("123    123ab123", 3, "|", true)), PHP_EOL;
+echo(wordwrap("123     123ab123", 3, "|", true)), PHP_EOL;
+echo(wordwrap("123      123ab123", 3, "|", true)), PHP_EOL;
+
+// handling newline:
+echo wordwrap("ab a\nbbbb\nccccc\nddd\neeee", 5), PHP_EOL;
+
+//
+echo "Done.";
