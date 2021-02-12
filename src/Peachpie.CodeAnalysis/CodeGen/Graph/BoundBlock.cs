df2266aa88ef0f0ef0a235fc97e55a00df34c975
Jakub Míšek <jmisek@gmail.com>
2021-02-12 11:50:29 +0100

    IndirectLocal only emited in debug and small amount of locals
    - otherwise might cause Stack Overflow Exception, especially in global code

--- src/Peachpie.CodeAnalysis/CodeGen/Graph/BoundBlock.cs
+++ src/Peachpie.CodeAnalysis/CodeGen/Graph/BoundBlock.cs
@@ -94,7 +94,10 @@ namespace Pchp.CodeAnalysis.Semantics.Graph
             }
 
             // emit dummy locals showing indirect (unoptimized) locals in debugger's Watch and Locals window
-            if (cg.HasUnoptimizedLocals && cg.EmitPdbSequencePoints && cg.IsDebug && cg.CoreTypes.IndirectLocal.Symbol != null)
+            if (cg.HasUnoptimizedLocals && cg.EmitPdbSequencePoints && cg.IsDebug &&
+                cg.CoreTypes.IndirectLocal.Symbol != null && // only if runtime provides this type (v1.0.0+)
+                cg.IsGlobalScope == false && // don't bother in global code
+                cg.Routine.LocalsTable.Count < 16) // emit IndirectLocal only there is not so many locals, otherwise it might use significant portion of stack
             {
                 EmitIndirectLocalsDebugWatch(cg);
             }
