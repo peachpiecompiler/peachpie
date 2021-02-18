a7e5ae325221276dfe99f71facb12d79e81cc8e0
Jakub Míšek <jmisek@gmail.com>
2021-02-18 16:31:50 +0100

    PHP 8 Attribute class
    ref https://github.com/peachpiecompiler/peachpie/issues/765

---
+++ src/Peachpie.Runtime/std/Attribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pchp.Core.Std
+{
+    /// <summary>
+    /// The Attribute class denotating a custom PHP attribute.
+    /// </summary>
+    [PhpType(PhpTypeAttribute.InheritName, MinimumLangVersion = "8.0"), PhpExtension("Core")]
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class Attribute : System.Attribute
+    {
+    }
+}
