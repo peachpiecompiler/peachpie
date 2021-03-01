5f1d61f1be252fb0d06a3cc1a8cff4b0f06f6785
Jakub Míšek <jmisek@gmail.com>
2021-03-01 15:28:19 +0100

    gd: correct resource type name

--- src/Peachpie.Library.Graphics/PhpGdImageResource.cs
+++ src/Peachpie.Library.Graphics/PhpGdImageResource.cs
@@ -20,6 +20,8 @@ namespace Peachpie.Library.Graphics
     /// </summary>
     internal class PhpGdImageResource : PhpResource
     {
+        const string ResourceTypeName = "gd";
+
         /// <summary>
         /// Underlying <see cref="Image"/> object.
         /// Cannot be <c>null</c> reference until it is not disposed.
@@ -66,7 +68,7 @@ namespace Peachpie.Library.Graphics
         //public bool BackgroundAllocated { get; set; }
 
         private PhpGdImageResource()
-            : base("GdImage")
+            : base(ResourceTypeName)
         {
         }
 
