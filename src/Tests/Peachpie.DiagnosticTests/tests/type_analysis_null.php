df0728dfa0332a8f8876471a2439b9d2ee084e12
Jakub Míšek <jmisek@gmail.com>
2021-04-12 19:35:35 +0200

    test update

--- src/Tests/Peachpie.DiagnosticTests/tests/type_analysis_null.php
+++ src/Tests/Peachpie.DiagnosticTests/tests/type_analysis_null.php
@@ -4,12 +4,16 @@ function foo1(): string {
     return "Hello";
 }
 
-function foo2(): ?string {
-    return "Hello";
+function foo2_todo(): ?string { // TODO: Nullable<PhpString>
+    return null;
+}
+
+function foo2(): ?stdClass {
+    return null;
 }
 
 /*|string|*/$res = foo1();
-/*|string|null|*/$res = foo2();
+/*|stdClass|null|*/$res = foo2();
 
 function foo3() { // : stdClass
     return new stdClass;
