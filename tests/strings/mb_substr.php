8e8bb79f093db0b9ca61b057ee7ad0a7a560291a
Jakub Míšek <jmisek@gmail.com>
2021-04-21 12:27:41 +0200

    test: mb_substr 8bit encoding
    ref https://github.com/peachpiecompiler/peachpie/issues/752

--- tests/strings/mb_substr.php
+++ tests/strings/mb_substr.php
@@ -7,5 +7,6 @@ echo mb_substr("hello", 1), "\n";
 echo mb_substr("hello", 1, NULL), "\n";
 echo mb_substr("hello", 1, -1), "\n";
 echo mb_substr("hello", 1, $null), "\n";
+echo mb_substr("\x88\x99\xccHello", 3, null, "8bit"), "\n";
 
 echo "Done.";
