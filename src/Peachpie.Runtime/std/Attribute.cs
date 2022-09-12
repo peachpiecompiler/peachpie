c6f92d76630c459b6f8b308b27f39c18c976bd5f
Jakub Míšek <jmisek@gmail.com>
2022-09-13 00:25:26 +0200

    Attribute:: constants and missing annotations

--- src/Peachpie.Runtime/std/Attribute.cs
+++ src/Peachpie.Runtime/std/Attribute.cs
@@ -9,7 +9,29 @@ namespace Pchp.Core.Std
     /// </summary>
     [PhpType(PhpTypeAttribute.InheritName, MinimumLangVersion = "8.0"), PhpExtension("Core")]
     [AttributeUsage(AttributeTargets.Class)]
+    [Attribute(Attribute.TARGET_CLASS)]
     public sealed class Attribute : System.Attribute
     {
+        public const int TARGET_CLASS = 1;
+        public const int TARGET_FUNCTION = 2;
+        public const int TARGET_METHOD = 4;
+        public const int TARGET_PROPERTY = 8;
+        public const int TARGET_CLASS_CONSTANT = 16;
+        public const int TARGET_PARAMETER = 32;
+        public const int TARGET_ALL = TARGET_CLASS|TARGET_FUNCTION|TARGET_METHOD|TARGET_PROPERTY|TARGET_CLASS_CONSTANT|TARGET_PARAMETER;
+
+        public const int IS_REPEATABLE = 64;
+
+        public int flags;
+
+        public Attribute(int flags = TARGET_ALL)
+        {
+            __construct(flags);
+        }
+
+        public void __construct(int flags = TARGET_ALL)
+        {
+            this.flags = flags;
+        }
     }
 }
